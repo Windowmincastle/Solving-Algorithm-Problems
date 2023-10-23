457a4d594bbc0a494ec6f7eab4e747128d0c98fe
Windowmincastle <kcm1042@naver.com>
2023-10-23 17:30:31 +0900

    SWEA 문제풀이

--- SWEA_D2/SWEA_1984.py
+++ SWEA_D2/SWEA_1984.py
@@ -3,11 +3,11 @@ from collections import deque
 T = int(input())
 for tc in range(1,T+1):
 
-    n = deque(map(int,input().split()))
-    q = deque(sorted(n))
+    n = deque(map(int,input().split())) # 덱 자료형으로 받는다
+    q = deque(sorted(n)) # 정렬한다
     result = 0
-    q.pop()
-    q.popleft()
+    q.pop() # 오른쪽 제거 = 가장 큰 값
+    q.popleft() # 왼쪽 제거 = 가장 작은 값
 
     result = sum(q)/len(q) #처음에 여기서 sum(q)/len(q) 결과를 int로 바꿔서 result에 저장하ㅗ
     #print에서 round를 써버리니까 문제가 생겻던 것 같다
