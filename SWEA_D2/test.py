59154738cd4c68ebc9b75f4cedead1396650dd4e
Windowmincastle <kcm1042@naver.com>
2023-10-26 01:28:01 +0900

    SWEA 문제풀이

--- SWEA_D2/test.py
+++ SWEA_D2/test.py
@@ -1,21 +1,24 @@
+import sys
+sys.stdin = open("input.txt", "r")
+def count(arr):
+    ret = 0
+    for lst in arr:
+        cnt = 0
+        for j in range(len(lst)):
+            if lst[j]:  # 값이 0이 아닌경우
+                cnt+=1
+            else:
+                if cnt == K:
+                    ret += 1
+                cnt = 0
+    return ret
+
 T = int(input())
 
-for tc in range(1,T+1):
+for test_case in range(1, T + 1):
+    N, K = map(int, input().split())
+    arr = [list(map(int, input().split())) + [0] for _ in range(N)] + [[0]*(N+1)] # 가로 세로에 패딩을 주는 방법
+    arr_t = list(map(list,zip(*arr))) # 전치 행렬을 만드는 방법
 
-    n = int(input())
-
-    data = [ [0] * n for _ in range(n)] #이중 리스트를 만들고
-
-    for i in range(n): #왼쪽을 다 1로 초기화하자
-        data[i][0] = 1
-
-    for i in range(1,n):
-        for j in range(1,n):
-            data[i][j] = data[i-1][j-1] + data[i-1][j] # 현재 위치의 왼쪽 위와 대각선 위( 리스트상 바로위)의 합을 저장
-
-    print(f"#{tc}") #테스트 케이스 수 출력
-    for i in range(1,n):
-        for j in range(1,n):#리스트의 모든 요소에 접근한다.
-            if data[i][j] != 0: # 현재 값이 0이 아닐때만 출력
-                print(f"{data[i][j]}",end=' ')
-        print() # 반복문 i,j 한행 끝나면 줄바꿈 해야함
+    ans = count(arr) + count(arr_t)
+    print(f'#{test_case} {ans}')
