c3eff68a46bf3972993972493d650851c70a718f
Windowmincastle <kcm1042@naver.com>
2023-10-27 00:24:27 +0900

    SWEA 문제풀이

--- SWEA_D3/SWEA_1209.py
+++ SWEA_D3/SWEA_1209.py
@@ -12,6 +12,7 @@ for _ in range(1,10+1):
     arr = [ list(map(int,input().split())) for _ in range(100)] # 100 x 100 이차원 배열
     arr_t = list(map(list,zip(*arr))) #전치 행렬로 만듬
     ans = 0 # 정답 저장할 변수.
+
     maxlst = [] # 행의 합, 열의 합, 대각선의 합 , 반대 대각선의 합을 저장할 리스트
 
     #행의 합
@@ -32,7 +33,7 @@ for _ in range(1,10+1):
         linesum += arr[k][l]
     arr.append(linesum)
 
-    #대각선을 역순으로 접근하기 위해 reversed
+    #대각선을 역순으로 처리하기 위해 reversed
     line = list(reversed(line))
     line2 = list(reversed(line2))
 
