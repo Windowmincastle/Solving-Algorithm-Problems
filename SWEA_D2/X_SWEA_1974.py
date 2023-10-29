12468815d5f3c7b177ef427e37de8c3484377a3f
Windowmincastle <kcm1042@naver.com>
2023-10-30 00:48:34 +0900

    SWEA 문제풀이

--- SWEA_D2/X_SWEA_1974.py
+++ SWEA_D2/X_SWEA_1974.py
@@ -3,6 +3,13 @@ SWEA_1974 스도쿠 검증
 https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5Psz16AYEDFAUq&categoryId=AV5Psz16AYEDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=2&pageSize=30&pageIndex=1
 
 '''
+
+'''
+아이디어는 잘 낸 거 같은데
+문법을 잘 활용하지 못 해서 구현할 접근을 하지 못 한 거 같다.
+리스트 슬라이싱을 잘 써야한다.
+다음에 꼭 다시 풀기 현재시각 - 10월 30일 월요일 새벽 1시
+'''
 import sys
 sys.stdin = open("input.txt", "r")
 
