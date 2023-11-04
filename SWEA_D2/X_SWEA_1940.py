b138df744a55568b21203bddfd643fb76da0b785
Windowmincastle <kcm1042@naver.com>
2023-11-05 03:01:06 +0900

    매일 알고리즘

---
+++ SWEA_D2/X_SWEA_1940.py
@@ -0,0 +1,11 @@
+'''
+SWEA D2 1940 가랏! RC카
+https://swexpertacademy.command/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5PjMgaALgDFAUq&categoryId=AV5PjMgaALgDFAUq&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=2&pageSize=30&pageIndex=1\
+
+다시 풀어보기 현재 11월 5일 - 노션에 정리햇음
+
+'''
+
+T = int(input())
+
+for tc in range(1,T+1):
