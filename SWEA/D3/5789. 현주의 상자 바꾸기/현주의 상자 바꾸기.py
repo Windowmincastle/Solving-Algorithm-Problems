5fcc7e8f0aaaa35043901b695eea6d6aaa363094
minseong <114567331+Windowmincastle@users.noreply.github.com>
2023-12-13 19:55:54 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 447 ms, Memory: 62,012 KB -BaekjoonHub

--- SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
@@ -1,16 +1,12 @@
-
 T = int(input())
 for tc in range(1,T+1):
 
     # N = 5 , K = 2
     N,Q = map(int,input().split())
-    lst = [0] * N
-
+    lst = [0] * (N+1)
     for i in range(1,Q+1):
-
         L,R = map(int,input().split())
-
         for j in range(L,R+1):
-            lst[j-1] = i
+            lst[j] = i
 
-    print(f'#{tc}',*lst)
+    print(f'#{tc}',*lst[1:])
