ed201e23c39eb81324ba737918c477d0a86dd357
minseong <114567331+Windowmincastle@users.noreply.github.com>
2023-12-25 12:52:47 +0900

    [Silver I] Title: 미로 탐색, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,13 +1,13 @@
-from collections import deque
-def bfs(si, sj, ei, ej):
-    q = deque()
+def bfs(si,sj,ei,ej):
+    #큐 생성 , 방문 처리 테이블 생성
+    q = []
     v = [[0]*M for _ in range(N)]
-
+    #큐에 삽입하고 방문처리
     q.append((si,sj))
     v[si][sj] = 1
-
+    # 큐가 빌때까지 반복
     while q:
-        ci, cj = q.popleft()
+        ci, cj = q.pop(0)
         if (ci,cj) == (ei,ej):  # 정답 처리가 필요한 경우 이자리에서..
             return v[ei][ej]
 
@@ -18,7 +18,7 @@ def bfs(si, sj, ei, ej):
                 q.append((ni,nj))
                 v[ni][nj] = v[ci][cj]+1
 
-N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
-ans = bfs(0, 0, N-1, M-1)
+N,M = map(int,input().split())
+arr = [list(map(int,input())) for _ in range(N)]
+ans = bfs(0,0,N-1,M-1)
 print(ans)
