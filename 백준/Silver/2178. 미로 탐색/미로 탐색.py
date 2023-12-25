7d3db1cf38783de76394f3f213ddfde8fb1a3ddc
minseong <114567331+Windowmincastle@users.noreply.github.com>
2023-12-25 13:00:36 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 110800 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,24 +1,33 @@
-def bfs(si,sj,ei,ej):
-    #큐 생성 , 방문 처리 테이블 생성
+# import sys
+# sys.stdin=open('input.txt','r')
+
+# 상 , 하 , 좌 , 우
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
+def bfs(si,sj):
     q = []
-    v = [[0]*M for _ in range(N)]
-    #큐에 삽입하고 방문처리
+    v = [[0] * M for _ in range(N)]
+
+    #큐에 넣고 방문처리
     q.append((si,sj))
     v[si][sj] = 1
-    # 큐가 빌때까지 반복
-    while q:
-        ci, cj = q.pop(0)
-        if (ci,cj) == (ei,ej):  # 정답 처리가 필요한 경우 이자리에서..
-            return v[ei][ej]
 
-        # 4방향, 범위내, 조건에 맞으면: arr == 1, v == 0
-        for di,dj in ((-1,0), (1,0), (0,-1), (0,1)):
-            ni, nj = ci+di, cj+dj
-            if 0<=ni<N and 0<=nj<M and arr[ni][nj]==1 and v[ni][nj]==0:
+    while q:
+
+        ci,cj = q.pop(0)
+        # if ci == N and cj == M:
+        #     return v[ci][cj]
+
+        for i in range(4):
+            ni = ci+di[i]
+            nj = cj+dj[i]
+            if 0 <= ni < N and 0 <= nj < M and v[ni][nj] == 0 and arr[ni][nj] == 1 :
                 q.append((ni,nj))
-                v[ni][nj] = v[ci][cj]+1
+                v[ni][nj] = v[ci][cj] + 1
+
+    return v[N-1][M-1]
 
 N,M = map(int,input().split())
 arr = [list(map(int,input())) for _ in range(N)]
-ans = bfs(0,0,N-1,M-1)
+ans = bfs(0,0)
 print(ans)
