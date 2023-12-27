d7903d42720cb063d63e5d9095fe4401ed6a62dd
minseong <114567331+Windowmincastle@users.noreply.github.com>
2023-12-28 08:24:31 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,42 +2,32 @@
 # sys.stdin=open('input.txt','r')
 
 def bfs(si,sj):
-    #집의 수
-    count = 0
     q = []
-    v = [[0]*N for _ in range(N)]
 
     q.append((si,sj))
-    v[si][sj] = 1 # 방문처리
-    count += 1
+    v[si][sj] = 1
+    cnt = 1
 
     while q:
-
         ci,cj = q.pop(0)
-
         for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
-            ni,nj = ci+di,cj+dj
-            if 0<=ni<N and 0<=nj<N and map[ni][nj] == 1 and v[ni][nj] == 0:
+            ni = ci + di
+            nj = cj + dj
+            if 0<=ni<N and 0<=nj<N and arr[ni][nj] == 1 and v[ni][nj] == 0:
                 q.append((ni,nj))
-                count += 1
-                map[ni][nj] = 2
                 v[ni][nj] = 1
-
-    return count
+                cnt += 1
+    return cnt
 
 N = int(input())
-map = [list(map(int,input())) for _ in range(N)]
-lst = []
-num = cnt = 0
+arr = [list(map(int,input())) for _ in range(N)]
+v = [[0]*N for _ in range(N)]
+ans = []
 
 for i in range(N):
     for j in range(N):
-        if map[i][j] == 1:
-            cnt = bfs(i,j)
-            num += 1
-            lst.append(cnt)
+        if arr[i][j]==1 and v[i][j]==0:
+            ans.append(bfs(i,j))
 
-print(num)
-lst.sort()
-for i in lst:
-    print(i)
+ans.sort()
+print(len(ans),*ans,sep='\n')
