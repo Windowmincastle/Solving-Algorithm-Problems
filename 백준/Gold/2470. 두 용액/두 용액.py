7cd2afdaa5164794a93f11d8c45786407a291184
minseong <114567331+Windowmincastle@users.noreply.github.com>
2023-12-28 14:17:34 +0900

    [Gold V] Title: 두 용액, Time: 128 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,33 +1,32 @@
-# import sys
-# sys.stdin=open('input.txt','r')
-'''
-참고 블로그
-https://velog.io/@kcs05008/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%91%90-%EC%9A%A9%EC%95%A1-%EB%B0%B1%EC%A4%80-2470-python
-'''
 n = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
+lst = list(map(int, input().split()))
+lst.sort()
 
-left = 0
-right = n - 1
+L = 0
+R = n-1
+#첫 값으로 맨 왼쪽과 오른쪽을 더한 값의 절대값을 넣어준다.
+tmp = abs(lst[L] + lst[R])
+#정답 처리를 위해서 현재 값의 쌍을 ans 리스트에 저장
+ans = [lst[L], lst[R]]
 
-answer = abs(arr[left] + arr[right])
-final = [arr[left], arr[right]]
-
-while left < right:
-    left_val = arr[left]
-    right_val = arr[right]
+while L < R:
+    left_val = lst[L]
+    right_val = lst[R]
 
     sum = left_val + right_val
 
-    if abs(sum) < answer:
-        answer = abs(sum)
-        final = [left_val, right_val]
-        if answer == 0:
+    # 현재 tmp 보다 작은 값을 찾으면 진입
+    if abs(sum) < tmp:
+        tmp = abs(sum)
+        ans = [left_val, right_val] # 정답처리를 위해서 현재보다 작은 값이 있다면 갱신
+        # tmp가 0이면 best case기 때문에 break
+        if tmp == 0:
             break
-    if sum < 0:
-        left += 1
-    else:
-        right -= 1
 
-print(final[0], final[1])
+    # 0에 가깝게 만들어주기 위해서 left와 right의 위치를 0을 기준으로 갱신해준다.
+    if sum < 0:
+        L += 1
+    else:
+        R -= 1
+
+print(ans[0], ans[1])
