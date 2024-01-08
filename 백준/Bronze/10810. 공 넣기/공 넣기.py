4f3d7220da7c35414377e23f417d96535ce1cf29
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-01-08 23:54:51 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,9 +1,10 @@
-N,M = map(int,input().split())
-lst = [0] * (N+1)
+N, M = map(int, input().split())
+basket = [0] * (N+1)
 
 for _ in range(M):
-    start , end , num = map(int,input().split())
-    for i in range(start,end+1):
-        lst[i] = num
+    i, j, k = map(int, input().split())
+    for n in range(i, j+1):
+        basket[n] = k 
 
-print(*lst[1:])
+for i in range(1, N+1):
+    print(basket[i], end = ' ')
