13de4cc9452d1af1a059bffbee6674a5d7833a05
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-01-13 22:10:40 +0900

    [level Lv.0] Title: 카운트 업, Time: 0.01 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/Lv.0/181920. 카운트 업/카운트 업.java
@@ -1,13 +1,12 @@
 class Solution {
+    
     public int[] solution(int start, int end) {
         
-        int[] answer = new int[end-start+1];
-        
-        for (int i=0; i<=end-start; i++)
-        {
-            answer[i] = i + start;
+        int[] answer = new int[end - start + 1];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = start + i;
         }
-        
         return answer;
     }
 }
