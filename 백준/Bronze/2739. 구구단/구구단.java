6301e9b1e09eacf4da2e93562c8b5c18188c4e58
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-05-28 19:32:12 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18640 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,17 +1,18 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.util.Scanner;
 
-public class Main{
-    
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        
-        int input = scan.nextInt();
-        
-        for(int i=1; i<10; i++)
-        {
-            System.out.println(input+" * "+ i +" = "+  input*i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i < 10; i++) {
+
+            System.out.println(n+" * "+i+" = "+n*i);
+
         }
     }
-    
 }
