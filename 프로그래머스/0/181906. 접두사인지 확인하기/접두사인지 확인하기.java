cf419d2b2a7d39eb52c472fe0737b3c1c5cf2827
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-06-01 19:11:46 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+        
+        // is_prefix가 my_string의 접두사인지 확인
+        if (my_string.startsWith(is_prefix)) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        
+        return answer;
+    }
+}
