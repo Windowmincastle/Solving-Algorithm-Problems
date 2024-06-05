412e1cec689dcc2203e98db54b07d443cf4ea611
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-06-06 01:50:18 +0900

    [Bronze IV] Title: 상근날드, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int buger1 = Integer.parseInt(br.readLine());
+        int buger2 = Integer.parseInt(br.readLine());
+        int buger3 = Integer.parseInt(br.readLine());
+
+        int col1 = Integer.parseInt(br.readLine());
+        int col2 = Integer.parseInt(br.readLine());
+
+
+        int buger = Math.min(buger1,buger2);
+        int buger_min = Math.min(buger,buger3);
+        int drink_min = Math.min(col1, col2);
+
+        int result = buger_min + drink_min - 50 ;
+        System.out.println(result);
+
+
+    }
+
+}
