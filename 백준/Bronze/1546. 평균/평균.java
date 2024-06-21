37d8b61a556c3dd2f840dedb690c26cbd66254c8
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-06-21 19:45:39 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 15336 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,44 +1,40 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import java.util.Arrays;
 
 public class Main {
-    
-    public static void main(String[] args){
-        
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        double maxValue = 0; // 수정: double로 선언
-        
-        // 성적 입력 받기.
-        double[] grade = new double[n];
-        double[] newGrade = new double[n];
-        
-        for (int i=0; i<n; i++){
-            grade[i] = input.nextInt();
-        }
-        
-        input.close(); // Scanner 객체 닫기
-        
-        maxValue = grade[0];
-        for (int i=1; i<n; i++){
-            if (grade[i] > maxValue){
-                maxValue = grade[i];
-            }
-        }
-        
-        // 원점수 / maxValue * 100
-        for (int i=0; i<n; i++){
-            newGrade[i] = grade[i] / maxValue * 100;
-        }
-        
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+
+        double score[] = new double[n];
         double avg = 0;
-        
-        for (int i=0; i<n; i++){
-            avg += newGrade[i];
+        double max = 0;
+        double sum = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            score[i] = Double.parseDouble(st.nextToken());
+            if (score[i] > max) max = score[i];
         }
-        
-        double answer = avg / n;
-        
-        // 수정: 새로운 평균을 출력
-        System.out.println(answer);
+
+        for (int i = 0; i < score.length; i++) {
+            score[i] = (score[i]/max) * 100;
+            sum += score[i];
+        }
+
+        avg = sum / n;
+        System.out.println(avg);
+
     }
 }
