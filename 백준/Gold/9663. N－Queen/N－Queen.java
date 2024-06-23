7f50d306d99089692c8e7208fe1a60bd976398e2
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-06-23 16:36:31 +0900

    [Gold IV] Title: N-Queen, Time: 2404 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +11,9 @@ public class Main {
     static int[] v2;
     static int[] v3;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
         ans = 0;
         v1 = new int[n]; // 열 체크 배열
         v2 = new int[2 * n]; // 좌측 대각선 체크 배열
@@ -21,32 +24,27 @@ public class Main {
 
         // 결과 출력
         System.out.println(ans);
-
-        // 스캐너 닫기
-       // sc.close();
     }
 
-    // 퀸을 배치하는 함수
-    static void dfs(int row) {
+    // 퀸을 배치 dfs 메서드 , c = col , r = row
+    static void dfs(int r) {
         // 모든 행에 퀸을 배치한 경우
-        if (row == n) {
-            // 유효한 배치의 수를 증가
+        if (r == n) {
+            // 경우의 수 증가
             ans++;
             return;
         }
 
         // 현재 행의 각 열에 대해 퀸을 놓을 수 있는지 체크
-        for (int col = 0; col < n; col++) {
-            // 현재 열과 대각선에 퀸이 없는 경우
-            if (v1[col] == 0 && v2[row + col] == 0 && v3[row - col + n] == 0) {
+        for (int c = 0; c < n; c++) {
+            // 현재 열과 좌우 대각선에 퀸이 없는 경우
+            if (v1[c] == 0 && v2[r + c] == 0 && v3[r - c + n] == 0) {
                 // 퀸을 해당 위치에 놓음
-                v1[col] = v2[row + col] = v3[row - col + n] = 1;
-
+                v1[c] = v2[r + c] = v3[r - c + n] = 1;
                 // 다음 행으로 이동하여 퀸을 배치 시도
-                dfs(row + 1);
-
+                dfs(r + 1);
                 // 현재 위치에서 퀸을 제거 (백트래킹)
-                v1[col] = v2[row + col] = v3[row - col + n] = 0;
+                v1[c] = v2[r + c] = v3[r - c + n] = 0;
             }
         }
     }
