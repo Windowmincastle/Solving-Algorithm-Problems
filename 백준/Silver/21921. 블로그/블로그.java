19d64ec1eb69844c913ce35489904b629d3a711c
minseong <114567331+Windowmincastle@users.noreply.github.com>
2024-07-13 22:19:37 +0900

    [Silver III] Title: 블로그, Time: 316 ms, Memory: 40356 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,38 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
         String[] firstLine = br.readLine().split(" ");
         int N = Integer.parseInt(firstLine[0]);
         int X = Integer.parseInt(firstLine[1]);
-        
         String[] input = br.readLine().split(" ");
         int[] ary = new int[N];
         for (int i = 0; i < N; i++) {
             ary[i] = Integer.parseInt(input[i]);
         }
 
-        // 초기 X일 동안의 합 계산
-        int currentSum = 0;
+        //초기 x일동안의 합을 계산
+        int curSum = 0;
         for (int i = 0; i < X; i++) {
-            currentSum += ary[i];
+            curSum += ary[i];
         }
 
-        int maxValue = currentSum;
+        int maxValue = curSum;
         int cnt = 1;
 
-        // 슬라이딩 윈도우
         for (int i = X; i < N; i++) {
-            currentSum = currentSum + ary[i] - ary[i - X];
-            if (currentSum > maxValue) {
-                maxValue = currentSum;
+
+            curSum = curSum + ary[i] - ary[i - X];
+
+            if (curSum > maxValue) {
                 cnt = 1;
-            } else if (currentSum == maxValue) {
+                maxValue = curSum;
+            } else if (curSum == maxValue) {
                 cnt++;
             }
         }
@@ -44,5 +46,7 @@ public class Main {
             sb.append(cnt);
             System.out.println(sb);
         }
+
     }
 }
+
