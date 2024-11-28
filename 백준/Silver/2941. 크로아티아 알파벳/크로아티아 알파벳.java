08c12daaf734af5dea3abdcc02f976d33c4c6ef5
Minseong <kcm1042@naver.com>
2024-11-28 13:43:05 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,26 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main {
-//  문제 유형 : 구현
-//  문제 난이도 : 실버 5
-//  문제 풀이 시간 : 1 시간 잡기.
+public class Main{
+
     public static void main(String[] args) throws Exception {
-
+    
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         String input = br.readLine();
-        String[] words = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-
-        for ( String word : words){
-
-            input = input.replace(word,",");
-
+        
+        String[] Alphabet = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        
+        for ( String a : Alphabet){
+        
+            input = input.replace(a,",");
+            
         }
         
         System.out.println(input.length());
-
-
+        
     }
 
 }
