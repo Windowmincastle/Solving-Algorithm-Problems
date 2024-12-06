efce651583c4c52145d880b5267f4432916eb27b
Minseong <kcm1042@naver.com>
2024-12-06 18:02:29 +0900

    [Silver V] Title: 집합, Time: 1516 ms, Memory: 307592 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,22 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
     public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int M = Integer.parseInt(br.readLine()); // 명령어 개수
-        HashSet<Integer> set = new HashSet<>(); // 집합 관리
-
-        for (int i = 0; i < M; i++) {
-            String[] input = br.readLine().split(" "); // 명령어 입력
-            String com = input[0]; // 명령어 추출
-
-            // 숫자가 필요한 명령어 처리
-            if (input.length == 2) {
+        
+        int M = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int i=0; i<M; i++) {
+            
+            String[] input = br.readLine().split(" ");
+            String com = input[0];
+            
+            if(input.length == 2){
+                
                 int x = Integer.parseInt(input[1]);
-
-                switch (com) {
+                
+                switch (com){
                     case "add":
                         set.add(x);
                         break;
@@ -28,20 +31,20 @@ public class Main {
                         sb.append(set.contains(x) ? "1\n" : "0\n");
                         break;
                     case "toggle":
-                        if (set.contains(x)) {
+                        if (set.contains(x)){
                             set.remove(x);
                         } else {
                             set.add(x);
                         }
                         break;
                 }
-            }
-            // 숫자가 필요 없는 명령어 처리
-            else if (input.length == 1) {
+                
+            } else if (input.length == 1) {
+                
                 switch (com) {
                     case "all":
                         set.clear();
-                        for (int j = 1; j <= 20; j++) {
+                        for (int j=1; j<=20; j++){
                             set.add(j);
                         }
                         break;
@@ -50,7 +53,8 @@ public class Main {
                         break;
                 }
             }
+            
         }
-        System.out.print(sb);
+        System.out.print(sb);        
     }
 }
