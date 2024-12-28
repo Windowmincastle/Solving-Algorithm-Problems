fde58bd4d5d701851655d7f661e75270a146398c
Minseong <kcm1042@naver.com>
2024-12-28 17:48:17 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,54 +1,47 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    /*
-     
-     */
-
-    public static void main(String[] args) throws Exception{
-
+public class Main{
+    
+    public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine()); // 사람 수
         
-        int[] tallerCount = new int[n]; // 자신보다 키큰 사람 수
-        int[] result = new int[n]; // 줄 선 순서.
+        int n = Integer.parseInt(br.readLine());
+        int[] tallerCnt = new int[n];
+        int[] answer = new int[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         
         for(int i=0; i<n; i++){
-            tallerCount[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for(int i=0; i<n; i++){
-            
-            int cnt = tallerCount[i];
-
-            for (int j = 0; j<n; j++){
-                // 빈 자리가 cnt만큼 지나가면 현재 사람을 배치한다.
-                if (cnt == 0 && result[j] == 0 ){
-                    
-                    result[j] = i+1;
-                    break;
-                }
-                //빈자리 발견시 cnt를 감소
-                if ( result[j] == 0 ){
-                    cnt--;
-                }
-            }
-
+            tallerCnt[i] = Integer.parseInt(st.nextToken());
         }
         
-        for (int r : result ) {
-            sb.append(r).append(" ");
-
+        for(int i=0; i<n; i++){
+            
+            int cnt = tallerCnt[i];
+            
+            for (int j=0; j<n; j++){
+                
+                if( cnt==0 && answer[j]==0){
+                    answer[j] = i+1;
+                    break;
+                }
+                
+                if(answer[j] == 0){
+                    cnt--;
+                }
+                
+            }
+            
         }
-
+        
+        for( int ans : answer ){
+            sb.append(ans).append(" ");
+        }
         System.out.println(sb);
-
+        
     }
+    
 }
