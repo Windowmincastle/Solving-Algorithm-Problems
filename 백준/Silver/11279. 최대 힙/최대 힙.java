672fd97342daf6e246301b16e23acd4208e2f568
Minseong <kcm1042@naver.com>
2025-01-29 14:22:57 +0900

    [Silver II] Title: 최대 힙, Time: 300 ms, Memory: 26992 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,32 +1,33 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+    
     public static void main(String[] args) throws Exception {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder()); // Long 타입 우선순위 큐
-
-        for (int i = 0; i < n; i++) { // 반복문 조건 수정
-            long x = Long.parseLong(br.readLine()); // x를 Long으로 입력받기
-
-            if (x > 0) { // 자연수라면 큐에 추가
+        
+        PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int i=0; i<n; i++){
+            
+            long x = Long.parseLong(br.readLine());
+            
+            if(x > 0 && x < Long.MAX_VALUE){ // x가 자연수라면 pq에 값을 넣는다.
                 pq.add(x);
             } else {
-                if (!pq.isEmpty()) {
-                    sb.append(pq.poll()).append("\n"); // 우선순위가 가장 높은 값 출력 후 제거
+                
+                if(!pq.isEmpty()){
+                    sb.append(pq.poll()).append("\n");
                 } else {
-                    sb.append(0).append("\n"); // 큐가 비어 있으면 0 출력
+                    sb.append(0).append("\n");
                 }
             }
         }
-
-        System.out.println(sb); // StringBuilder 출력
+      
+        System.out.println(sb);
     }
+    
 }
