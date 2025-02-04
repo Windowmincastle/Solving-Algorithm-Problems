aad72c774fe30fa59e2cea929aed7c81feb1f99b
Minseong <kcm1042@naver.com>
2025-02-04 20:14:35 +0900

    [level 2] Title: H-Index, Time: 0.85 ms, Memory: 70 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        
+        for(int i=0; i<n; i++){
+            
+            int h = n - i;
+            
+            if(citations[i] >= h){
+                return h;
+            }
+        }
+        
+        
+        return 0;
+    }
+}
