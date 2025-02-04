0631cdb929f3a18043bdcf90b11f1c4432de3ee5
Minseong <kcm1042@naver.com>
2025-02-04 22:52:33 +0900

    [level 1] Title: 체육복, Time: 0.16 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 77 MB, 시간: 0.64 ms
+메모리: 89.4 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 26일 12:08:21
+2025년 02월 04일 22:52:28
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,62 +1,56 @@
+import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-
-        int answer = 0;
-        //앞번호 or 뒷번호 학생에게만 체육복을 빌려줌
-        //최대한 많은 학생이 체육 수업을 들어야함
-
-//        전체 학생의 수 n
-//        체육복을 도난당한 학생들의 번호가 담긴 배열 lost
-//        여벌의 체육복을 가져온 학생들의 번호 배열 reserve
-//        체육 수업을 들을 수 잇는 학생의 최댓값을 반환
-
-//        여벌 체육복을 가져온 학생이 도난을 당햇을 수 있다. 그러면 체육복을 빌려줄 수 없다
-
-
-        Set<Integer> resSet = new HashSet<>();
-        Set<Integer> losSet = new HashSet<>();
-
-//      Set에 담기
-
-        Arrays.sort(reserve);
-//        for (int i : reserve) {
-//            resSet.add(i);
-//        }
-
-        for (int i : lost) {
-            losSet.add(i);
+        
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
+        
+        // lost에서 lostSet으로 체육복을 잃어버린 학생들을 저장.
+        for (int l : lost){
+            
+            lostSet.add(l);
+            
         }
-
-        //asdf
-        for (int i = 0; i < reserve.length; i++) {
-            if (losSet.contains(reserve[i])) {
-                losSet.remove(reserve[i]);
-                reserve[i] = -1;
+        
+        // 체육복을 빌려줄 수 있는 사람일 경우라도 도난을 당했다면 자신의 체육복을 입어야하기 때문에 lostSet에서 contains로 해당 경우를 remove하기
+        for (int r : reserve) {
+            
+            if (lostSet.contains(r)) {
+                
+                lostSet.remove(r);
+                
+            } else {
+                //그런 경우가 아니라면 빌려줄 수 있기 때문에 add
+                reserveSet.add(r);
+                
             }
+            
         }
-
-        for (int i = 0; i < reserve.length; i++) {
-            if (reserve[i] == -1) continue;
-
-            if (losSet.contains(reserve[i] - 1)) {
-                losSet.remove(reserve[i] - 1);
-            } else if (losSet.contains(reserve[i]+1)) {
-                losSet.remove(reserve[i] + 1);
+        
+        // 빌려주기
+        
+        int ableToAttend = n - lostSet.size();
+        List<Integer> lostList = new ArrayList<>(lostSet);
+        
+        for (int l : lostList){
+            
+            if(reserveSet.contains(l-1)){
+                
+                reserveSet.remove(l-1);
+                ableToAttend++;
+                
+            } else if (reserveSet.contains(l + 1)){
+                
+                reserveSet.remove(l+1);
+                ableToAttend++;
             }
+            
         }
-//
-//        for (int j : resSet) {
 
-//            if (losSet.contains(j - 1)) {
-//                losSet.remove(j - 1);
-//            } else if (losSet.contains(j+1)) {
-//                losSet.remove(j + 1);
-//            }
-//        }
-
-        answer = n-losSet.size();
-        return answer;
+        
+        
+        return ableToAttend;
     }
 }
