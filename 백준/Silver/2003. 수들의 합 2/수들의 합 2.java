575f05ba1f87526f7b1b6f0ccd7cdd9c22e000ca
Minseong <kcm1042@naver.com>
2025-03-02 15:03:47 +0900

    [Silver IV] Title: 수들의 합 2, Time: 128 ms, Memory: 14972 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,46 +1,47 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 입력 받기
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 수열의 길이
-        int m = Integer.parseInt(st.nextToken()); // 목표 합
-
-        int[] arr = new int[n]; // 수열을 저장할 배열
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken()); // 배열 초기화
+        
+        for(int i=0; i<n; i++) {
+            
+            arr[i] = Integer.parseInt(st.nextToken());
+            
         }
-
-        // **두 포인터 알고리즘 시작**
+        
         int left = 0, right = 0;
-        int sum = 0; // 부분 합
-        int count = 0; // 조건을 만족하는 경우의 수
-
-        while (right < n) {
-            // sum을 증가시키면서 right 포인터를 이동
+        int sum = 0;
+        int count = 0;
+        
+        while(right<n){
+            
+            
             sum += arr[right];
-
-            // sum이 목표값 M을 초과하면 left 포인터를 이동하며 조정
-            while (sum > m && left <= right) {
-                sum -= arr[left]; // left 위치 값을 빼고
-                left++; // left 포인터 증가
+            
+            while(sum > m && left <= right ){
+                sum -= arr[left];
+                left++;
             }
-
-            // 조건을 만족하면 count 증가
-            if (sum == m) {
+            
+            if(sum == m){
                 count++;
             }
-
-            right++; // right 포인터 이동
+            
+            right++;
         }
-
-        // 결과 출력
+        
         System.out.println(count);
-        br.close();
     }
+    
 }
