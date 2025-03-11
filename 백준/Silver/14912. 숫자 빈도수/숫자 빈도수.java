873d36cb0689395f8e81576cc147c96e948a0337
Minseong <kcm1042@naver.com>
2025-03-11 21:28:01 +0900

    [Silver V] Title: 숫자 빈도수, Time: 152 ms, Memory: 22976 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
@@ -1,21 +1,25 @@
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-
+        
         int n = Integer.parseInt(input[0]);
-        char d = input[1].charAt(0); // char로 변환
-
+        char d = input[1].charAt(0);
+        
         int count = 0;
-        for (int i = 1; i <= n; i++) {
-            String numStr = String.valueOf(i); // 숫자를 문자열로 변환
-            for (char c : numStr.toCharArray()) {
+        
+        for(int i= 1; i <= n; i++) {
+            
+            String numStr = String.valueOf(i);
+            for (char c : numStr.toCharArray()){
                 if (c == d) count++;
             }
+            
         }
-
-        System.out.println(count);
+        System.out.println(count);        
     }
 }
