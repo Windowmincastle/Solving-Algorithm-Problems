d02d5533465506fbbb62e88fcc6da6fb6faab63d
Minseong <kcm1042@naver.com>
2025-03-16 17:47:46 +0900

    [Silver V] Title: 브실이의 입시전략, Time: 256 ms, Memory: 21740 KB -BaekjoonHub

--- 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.java
+++ 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.java
@@ -1,53 +1,54 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 입력 처리
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int K = sc.nextInt();
-        sc.nextLine(); // 개행 처리
+        // N: 과목 수, M: 선택할 과목 수, K: 공개된 과목 수
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
         // 과목 점수 저장
         Map<String, Integer> subjectScores = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            String[] input = sc.nextLine().split(" ");
-            String subject = input[0];
-            int score = Integer.parseInt(input[1]);
+            st = new StringTokenizer(br.readLine());
+            String subject = st.nextToken();
+            int score = Integer.parseInt(st.nextToken());
             subjectScores.put(subject, score);
         }
 
         // 공개된 과목 저장
         Set<String> knownSubjects = new HashSet<>();
         for (int i = 0; i < K; i++) {
-            knownSubjects.add(sc.nextLine());
+            knownSubjects.add(br.readLine());
         }
 
-        // 공개된 과목 점수 합산
-        List<Integer> knownScores = new ArrayList<>();
+        // 공개된 과목 점수 합산 & 비공개 과목 점수 리스트 저장
         List<Integer> unknownScores = new ArrayList<>();
+        int knownTotalScore = 0;
 
         for (Map.Entry<String, Integer> entry : subjectScores.entrySet()) {
             if (knownSubjects.contains(entry.getKey())) {
-                knownScores.add(entry.getValue());
+                knownTotalScore += entry.getValue();
             } else {
                 unknownScores.add(entry.getValue());
             }
         }
 
-        // 비공개 과목 점수 정렬
+        // 비공개 과목 점수 정렬 (오름차순)
         Collections.sort(unknownScores);
 
         // 최소 점수 계산: 가장 낮은 비공개 과목 점수 M-K개 추가
-        int minScore = knownScores.stream().mapToInt(Integer::intValue).sum();
+        int minScore = knownTotalScore;
         for (int i = 0; i < M - K; i++) {
             minScore += unknownScores.get(i);
         }
 
         // 최대 점수 계산: 가장 높은 비공개 과목 점수 M-K개 추가
-        int maxScore = knownScores.stream().mapToInt(Integer::intValue).sum();
+        int maxScore = knownTotalScore;
         for (int i = 0; i < M - K; i++) {
             maxScore += unknownScores.get(unknownScores.size() - 1 - i);
         }
