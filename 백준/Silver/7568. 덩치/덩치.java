c5e603ad570b28c759ae68917f82a27bd489e5f6
Minseong <kcm1042@naver.com>
2025-06-18 21:35:35 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,57 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-class Person {
-    
-    int w;
-    int h;
-    int rank;
-    
-    public Person (int w, int h){
-        this.w = w;
-        this.h = h;
-        this.rank = 1;
-    }
-}
-
-public class Main {
+public class Main{
     
     public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
-        List<Person> pp = new ArrayList<>();
-        
+        int m = 2;
+        int[][] ary = new int[n][m];
+
         for (int i=0; i<n; i++){
-            
+
             String[] input = br.readLine().split(" ");
             
-            int w = Integer.parseInt(input[0]);
-            int h = Integer.parseInt(input[1]);
-            
-            pp.add(new Person(w,h));
+            ary[i][0] = Integer.parseInt(input[0]);
+            ary[i][1] = Integer.parseInt(input[1]);
+        
         }
         
-        for (int i=0; i<pp.size(); i++){
-            
-            for(int j=0; j<pp.size(); j++){
-                
-                if(i!=j){
-                    if(pp.get(i).w < pp.get(j).w && pp.get(i).h < pp.get(j).h){
-                        pp.get(i).rank++;
-                    }
-                }
-            }
-        }
-     
         StringBuilder sb = new StringBuilder();
         
-        for (Person p : pp ){
+        for (int j=0; j<n; j++){
+
+            int count = 1;    
             
-            sb.append(p.rank).append(" ");
+            for (int k=0; k<n; k++){
+                
+                if( ary[j][0] < ary[k][0] && ary[j][1] < ary[k][1] ){
+                    count++;
+                }
+            }
+            sb.append(count).append(" ");
         }
         
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
