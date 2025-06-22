f7b597d96e35775d08e825a21c9a6f182b94663d
Minseong <kcm1042@naver.com>
2025-06-22 19:28:58 +0900

    [Silver IV] Title: 한수, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,31 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        System.out.println(countHanNumbers(N));
-    }
-
-    // 한수의 개수를 세는 메서드
-    public static int countHanNumbers(int N) {
-        if (N < 100) return N; // 1~99는 모두 한수이므로 N 그대로 반환
-
-        int count = 99; // 1~99는 모두 한수
-        for (int i = 100; i <= N; i++) {
-            if (isHanNumber(i)) count++;
+        
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        
+        for(int i=1; i<=n; i++){
+            
+            if( i < 100 ){
+                count++;
+                continue;
+            }
+            
+            String str = String.valueOf(i);
+            
+            int[] numbers = new int[str.length()];
+            
+            for(int j = 0; j< str.length(); j++){
+                
+                numbers[j] = str.charAt(j) - '0';
+            }
+            
+            int standard = numbers[1] - numbers[0];
+            for (int k = 2; k<str.length(); k++){
+                
+                if ( numbers[k] - numbers[k-1] == standard ){
+                    count++;
+                }
+            }
         }
-        return count;
+        System.out.println(count);
     }
-
-    // 특정 숫자가 한수인지 판별하는 메서드
-    public static boolean isHanNumber(int num) {
-        int A = num / 100;         // 백의 자리
-        int B = (num / 10) % 10;   // 십의 자리
-        int C = num % 10;          // 일의 자리
-
-        return (B - A) == (C - B); // 등차수열 여부 확인
-    }
+    
 }
