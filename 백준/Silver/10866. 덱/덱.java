da5d0e61977d9e966db3363a68f2a3a8e74383e1
Minseong <kcm1042@naver.com>
2025-06-27 18:41:46 +0900

    [Silver IV] Title: 덱, Time: 304 ms, Memory: 20784 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,47 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Deque<String> deque = new ArrayDeque<>();
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            String[] com = br.readLine().split(" ");
-            switch (com[0]) {
-                case "push_front":
-                    deque.addFirst(com[1]);
-                    break;
-                case "push_back":
-                    deque.addLast(com[1]);
-                    break;
-                case "pop_front":
-                    sb.append(deque.isEmpty() ? "-1" : deque.pollFirst()).append("\n");
-                    break;
-                case "pop_back":
-                    sb.append(deque.isEmpty() ? "-1" : deque.pollLast()).append("\n");
-                    break;
-                case "size":
-                    sb.append(deque.size()).append("\n");
-                    break;
-                case "empty":
-                    sb.append(deque.isEmpty() ? "1" : "0").append("\n");
-                    break;
-                case "front":
-                    sb.append(deque.isEmpty() ? "-1" : deque.peekFirst()).append("\n");
-                    break;
-                case "back":
-                    sb.append(deque.isEmpty() ? "-1" : deque.peekLast()).append("\n");
-                    break;
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for (int i=0; i<n; i++){
+            
+            String[] input = br.readLine().split(" ");
+            if (input[0].equals("push_front")){
+                
+                deque.addFirst(Integer.parseInt(input[1]));
+                
+            } else if (input[0].equals("push_back")){
+                
+                deque.addLast(Integer.parseInt(input[1]));
+            
+            } else if (input[0].equals("pop_front")){
+                
+                if (deque.size() == 0) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(deque.removeFirst());
+                }
+            
+            } else if (input[0].equals("size")) {
+                System.out.println(deque.size());
+            } else if (input[0].equals("empty")){
+                
+                if (deque.size() == 0 ){
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
+                }
+                
+            } else if (input[0].equals("front")){
+                if ( deque.size()==0) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(deque.peekFirst());
+                }
+            } else if (input[0].equals("back")){
+                                if ( deque.size()==0) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(deque.peekLast());
+                }
+            } else if (input[0].equals("pop_back")){
+                
+                if (deque.size() == 0){
+                    System.out.println(-1);
+                } else {
+                    System.out.println(deque.removeLast());
+                }
             }
         }
-        System.out.print(sb.toString());
     }
 }
