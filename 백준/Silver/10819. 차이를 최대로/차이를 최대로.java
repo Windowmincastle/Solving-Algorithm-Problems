15dd87413da526fd995e53c4ea709f26557b8161
Minseong <kcm1042@naver.com>
2025-06-30 17:21:04 +0900

    [Silver II] Title: 차이를 최대로, Time: 132 ms, Memory: 15272 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,49 +1,56 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[] arr;
-    static boolean[] visited;
-    static List<Integer> permutedList;
-    static int maxResult = Integer.MIN_VALUE;
+    static int[] A;
+    static boolean[] used;
+    static int[] perm;
+    static int maxSum = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        arr = new int[N];
-        visited = new boolean[N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        // 입력
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        used = new boolean[N];
+        perm = new int[N];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        permutedList = new ArrayList<>();
-        backtrack(0);
-        System.out.println(maxResult);
+        // 순열 생성 시작
+        dfs(0);
+
+        // 결과 출력
+        System.out.println(maxSum);
     }
 
-    static void backtrack(int depth) {
+    // depth: 현재 perm에 채워진 개수
+    private static void dfs(int depth) {
         if (depth == N) {
-            maxResult = Math.max(maxResult, calculateSum());
+            // 완성된 순열의 차이 합 계산
+            int sum = 0;
+            for (int i = 0; i < N - 1; i++) {
+                sum += Math.abs(perm[i] - perm[i + 1]);
+            }
+            maxSum = Math.max(maxSum, sum);
             return;
         }
 
         for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                permutedList.add(arr[i]);
-                backtrack(depth + 1);
-                permutedList.remove(permutedList.size() - 1);
-                visited[i] = false;
+            if (!used[i]) {
+                used[i] = true;
+                perm[depth] = A[i];
+                dfs(depth + 1);
+                used[i] = false;
             }
         }
-    }
-
-    static int calculateSum() {
-        int sum = 0;
-        for (int i = 0; i < N - 1; i++) {
-            sum += Math.abs(permutedList.get(i) - permutedList.get(i + 1));
-        }
-        return sum;
     }
 }
