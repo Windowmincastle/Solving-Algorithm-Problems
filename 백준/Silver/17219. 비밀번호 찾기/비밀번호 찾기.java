90e92eb5d1be48d45e1c9018bbf67c9e67f740bb
Minseong <kcm1042@naver.com>
2025-07-05 20:02:44 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 608 ms, Memory: 64564 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -2,32 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    
     public static void main(String[] args) throws Exception {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
+        
         String[] input = br.readLine().split(" ");
-
+        // 저장된 사이트 주소의 수 N
         int n = Integer.parseInt(input[0]);
+        // 비밀번호를 찾으려는 사이트 주소의 수 M
         int m = Integer.parseInt(input[1]);
-
-        Map<String, String> site = new HashMap<>();
-
-        // N개의 사이트와 비밀번호 저장
-        for (int i = 0; i < n; i++) {
-            String[] ip = br.readLine().split(" ");
-            site.put(ip[0], ip[1]);
+        
+        // N개의 줄에 걸쳐 각 줄에 사이트 주소와 비밀번호가 공백으로 주어짐.
+        Map<String,String> saves = new HashMap<>();
+        
+        for (int i=0; i<n; i++){
+            
+            String[] siteAndpassword = br.readLine().split(" ");
+            String site = siteAndpassword[0];
+            String password = siteAndpassword[1];
+            
+            saves.put(site,password);
         }
-
-        // M개의 비밀번호 찾기
-        for (int i = 0; i < m; i++) {
-            String search = br.readLine();
-            sb.append(site.get(search)).append("\n");
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int j=0; j<m; j++){
+            
+            String target = br.readLine();
+            sb.append(saves.get(target)).append("\n");
+            
         }
-
-        // 결과 출력
-        System.out.print(sb);
+        
+        System.out.println(sb.toString());
+        
     }
+    
 }
