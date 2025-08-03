45befa600b2b936762768ac8663184483d6112ee
Minseong <kcm1042@naver.com>
2025-08-03 18:39:22 +0900

    [Silver IV] Title: 제로, Time: 196 ms, Memory: 24040 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,30 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int solution(BufferedReader br, int k) throws Exception {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int K = Integer.parseInt(br.readLine());
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        for (int i = 0; i < K; i++) {
-            int input = Integer.parseInt(br.readLine());
-            if (input == 0) {
-                if (!deque.isEmpty()) deque.removeLast();
-            } else{
-                deque.add(input);
+        Deque<Integer> list = new ArrayDeque<>();
+        
+        for(int i=0; i<k; i++){
+            
+            int temp = Integer.parseInt(br.readLine());
+            
+            if ( temp != 0){
+                list.add(temp);
+            } else {
+                list.removeLast();
             }
         }
-
-        int answer = 0;
-        for (int i : deque) {
-            answer += i;
+        int total = 0;
+        for ( int x : list){
+            total += x;
         }
+        return total;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        int answer = solution(br,k);
         System.out.println(answer);
-
     }
 }
