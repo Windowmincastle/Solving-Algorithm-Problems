66e874a36d9c352e3db80161eb18dc20280fc0e9
Minseong <kcm1042@naver.com>
2025-08-08 17:51:18 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 120 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,47 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main{
-    
-    public static void main(String[] args) throws Exception {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] input = br.readLine().split(" ");
-        
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
-        
-        Queue<Integer> queue = new LinkedList<>();
-        
-        for (int i=1; i <= n; i++){
-            
-            queue.offer(i);
-        
-        }
-        
+public class Main {
+
+    static String solution(int n, int k) {
         StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        
-        while( !queue.isEmpty()){
-            
-            for(int i=0; i<k-1; i++){
-                
-                queue.offer(queue.poll());            
-            
-            }
-            
-            sb.append(queue.poll());
-        
-            if (!queue.isEmpty()){
-                sb.append(", ");
-            }
-        
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        for (int i = 1; i <= n; i++) {
+            deque.addLast(i);
         }
-        
+
+        sb.append("<");
+        while (!deque.isEmpty()) {
+            // 현재 남은 사람 수만큼 나머지 연산으로 회전 횟수 줄이기
+            int rotate = (k - 1) % deque.size();
+            for (int j = 0; j < rotate; j++) {
+                deque.offerLast(deque.pollFirst());
+            }
+
+            // K번째 사람 제거
+            sb.append(deque.pollFirst());
+            if (!deque.isEmpty()) sb.append(", ");
+        }
         sb.append(">");
-        System.out.println(sb);
-        
+
+        return sb.toString();
     }
-    
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(solution(n, k));
+    }
 }
