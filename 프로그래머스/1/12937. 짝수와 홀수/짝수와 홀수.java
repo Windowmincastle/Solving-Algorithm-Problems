e320074d84a298123b5b69aaa361b7cfa3e56676
Minseong <kcm1042@naver.com>
2025-08-18 22:20:15 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,16 +1,18 @@
 class Solution {
+    
+    static String isEvenAndOdd(int number) {
+
+        String even = "Even";
+        String odd = "Odd";
+        
+        if ( number%2 == 0 ) {
+            return even;
+        } else {
+            return odd;
+        }
+    }
+    
     public String solution(int num) {
-        
-//         String answer = "";
-        
-//         if ( num % 2 == 0 ){
-//             answer = "Even";
-//         } else if (num % 2 == 1) {
-//             answer = "Odd";
-//         } else if ( num == 0 ){
-//             answer = "Even";
-//         }
-        
-        return (num % 2 == 0) ? "Even" : "Odd";
+        return isEvenAndOdd(num);
     }
 }
