b88fcb39236d507f77474dc75ef633e283717b54
Minseong <kcm1042@naver.com>
2025-08-31 19:35:42 +0900

    [level 2] Title: 최솟값 만들기, Time: 7.14 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,22 +1,46 @@
 import java.io.*;
 import java.util.*;
 
+
 class Solution
 {
-    public int solution(int []A, int []B)
-    {
-        Arrays.sort(A);
-        Arrays.sort(B);
+    static Integer[] boxing (int[] ary){
         
-        int minValue = 0;
+        Integer[] boxingAry = new Integer[ary.length];
         
-        for (int i=0; i< A.length; i++){
+        for (int i=0; i< ary.length; i++){
             
-            minValue += A[i] * B[B.length - 1 - i];
-            
+            boxingAry[i] = ary[i];
         }
         
+        return boxingAry;
+    }
+    
+    static int calculate(Integer[] a, Integer[] b){
+        
+        int ans = 0;
+        int len = a.length;
+        
+        for (int i=0; i<len; i++){
+            ans += a[i] * b[i];
+        }
+        
+        return ans;
+    }
+    
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        
+        Integer[] convertA = boxing(A);
+        Integer[] convertB = boxing(B);
+        
+        Arrays.sort(convertA);
+        Arrays.sort(convertB,Collections.reverseOrder());
+        
+        answer = calculate(convertA,convertB);
+        
 
-        return minValue;
+        return answer;
     }
 }
