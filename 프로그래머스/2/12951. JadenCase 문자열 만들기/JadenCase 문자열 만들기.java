46c50d4bf828285ac90d8f743c900ac982f0e623
Minseong <kcm1042@naver.com>
2025-09-04 17:22:47 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.21 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,22 +1,35 @@
 class Solution {
     public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
         
-        StringBuilder result = new StringBuilder();
-        boolean isStartOfWord = true; // 단어 시작 여부를 확인하는 플래그
+        // split(" ", -1) -> 연속된 공백도 보존
+        String[] words = s.split(" ", -1);
 
-        for (char c : s.toCharArray()) {
-            if (c == ' ') { // 공백인 경우
-                result.append(c);
-                isStartOfWord = true; // 다음 문자는 단어의 시작
-            } else if (isStartOfWord) { // 단어의 시작인 경우
-                result.append(Character.toUpperCase(c)); // 대문자로 변환
-                isStartOfWord = false; // 단어의 시작 상태 해제
-            } else { // 단어 중간인 경우
-                result.append(Character.toLowerCase(c)); // 소문자로 변환
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            
+            if (word.length() > 0) {
+                char first = word.charAt(0);
+                if (Character.isDigit(first)) {
+                    // 숫자로 시작하면: 첫 글자는 그대로 두고 나머지는 소문자
+                    answer.append(first);
+                    if (word.length() > 1) {
+                        answer.append(word.substring(1).toLowerCase());
+                    }
+                } else {
+                    // 알파벳으로 시작하면: 첫 글자는 대문자, 나머지는 소문자
+                    answer.append(Character.toUpperCase(first));
+                    if (word.length() > 1) {
+                        answer.append(word.substring(1).toLowerCase());
+                    }
+                }
+            }
+            
+            if (i < words.length - 1) {
+                answer.append(" ");
             }
         }
-
-        return result.toString();
         
+        return answer.toString();
     }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.12 ms
+메모리: 68.8 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 18일 16:29:48
+2025년 09월 04일 17:22:43
 
 ### 문제 설명
 
