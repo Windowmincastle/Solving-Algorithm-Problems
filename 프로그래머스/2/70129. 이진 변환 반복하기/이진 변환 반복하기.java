caec3718e9744f7b076026a557a2c6bd82603399
Minseong <kcm1042@naver.com>
2025-09-07 22:15:43 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 16.91 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,23 +1,42 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
+    
+    public static String getBinary(int input){
+        
+        String res = Integer.toBinaryString(input);
+        return res;
+    }
+    
+    public static String getRemoveZeroString(String str){
+        
+        String res = str.replace("0","");
+        
+        return res;
+    }
+    
     public int[] solution(String s) {
         
-        int convertCnt = 0;
-        int removeCnt = 0;
+        int count = 0;
+        int removeCount = 0;
+        int[] answer = new int[2];
         
-        while (!s.equals("1")) {
+        while (!s.equals("1")){
             
-            convertCnt++;
+            // 문자열 s가 1이 아니라면 진입했겟지.
+            count++; // 회차 카운트
+            removeCount += s.length() - s.replace("0","").length(); // 제거할 0 의 개수 저장.
             
-            int orgLen = s.length();
-            s = s.replace("0","");
-            int newLen = s.length();
-            
-            removeCnt += (orgLen - newLen);
-            
-            s = Integer.toBinaryString(newLen);
+            s = getRemoveZeroString(s); // 0을 제거하고
+            int temp = s.length(); // 제거후 문자열의 길이를 임시 저장후
+            s = getBinary(temp); // 그 길이를 이진수로 변환해서 s에 다시 저장.
             
         }
+        //탈출했을때
+        answer[0] = count;
+        answer[1] = removeCount;
         
-        return new int[] {convertCnt, removeCnt};
+        return answer;
     }
 }
