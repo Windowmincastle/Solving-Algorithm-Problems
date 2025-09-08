01bba5ca3e2214ffa5df6920a262bca9a5627a53
Minseong <kcm1042@naver.com>
2025-09-08 22:19:56 +0900

    [level 2] Title: 숫자의 표현, Time: 1.54 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,25 +1,23 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] arr = new int[n + 1];
-        for (int i = 0; i <= n; i++) { // 0 ~ 15
-            arr[i] = i;
-        }
 
-        int start = 1; int end = 1;
-        int sum = arr[1];
-        while (start <= end) {
-            if (sum == n) {
-                sum -= arr[start];
-                start++; answer++;
-            } else if (sum < n && end < arr.length - 1) {
-                end++;
-                sum += arr[end];
-            } else {
-                sum -= arr[start];
-                start++;
+        // 시작 숫자를 1부터 n까지 돌림
+        for (int start = 1; start <= n; start++) {
+            int sum = 0;
+            // start부터 연속해서 더해가기
+            for (int num = start; num <= n; num++) {
+                sum += num;
+                if (sum == n) {   // 합이 정확히 n이 되면 방법 1개 찾음
+                    answer++;
+                    break;       // 더 이상 볼 필요 없음 → 다음 start로
+                }
+                if (sum > n) {   // 합이 n을 넘으면 중단
+                    break;
+                }
             }
         }
+
         return answer;
     }
 }
