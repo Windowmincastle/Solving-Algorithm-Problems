772a88bc9331848410380968036ad3f49d4d5bcc
Minseong <kcm1042@naver.com>
2025-10-02 16:52:51 +0900

    [level 2] Title: 구명보트, Time: 10.45 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,30 +2,23 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        // 사람들의 몸무게 배열을 정렬합니다.
-        Arrays.sort(people);
+        Arrays.sort(people); // 몸무게 오름차순 정렬
+        int left = 0;                // 가장 가벼운 사람 인덱스
+        int right = people.length - 1; // 가장 무거운 사람 인덱스
+        int answer = 0;
 
-        // 두 포인터를 설정합니다.
-        int left = 0; // 가장 가벼운 사람을 가리키는 포인터
-        int right = people.length - 1; // 가장 무거운 사람을 가리키는 포인터
-        int boats = 0; // 필요한 구명보트의 개수
-
-        // 포인터가 서로 교차하기 전까지 반복합니다.
         while (left <= right) {
-            // 가장 가벼운 사람과 가장 무거운 사람의 무게 합이 제한을 넘지 않는다면
             if (people[left] + people[right] <= limit) {
-                // 두 사람을 한 보트에 태웁니다.
+                // 둘이 같이 탈 수 있으면
                 left++;
                 right--;
             } else {
-                // 무게 합이 제한을 넘는다면 가장 무거운 사람만 태웁니다.
+                // 무거운 사람만 태움
                 right--;
             }
-            // 보트를 사용했으므로 보트 개수를 증가시킵니다.
-            boats++;
+            answer++;
         }
 
-        // 필요한 구명보트의 최소 개수를 반환합니다.
-        return boats;
+        return answer;
     }
 }
