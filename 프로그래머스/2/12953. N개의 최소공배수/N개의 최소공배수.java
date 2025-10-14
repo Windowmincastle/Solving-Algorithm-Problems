3f946330d85214311f187c304eb4830046a3ce19
Minseong <kcm1042@naver.com>
2025-10-14 23:03:33 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,33 +1,26 @@
 class Solution {
-    
-    //최대공약수를 구하는 유클리드 알고리즘
-    public int getGCD(int a,int b){
-        
-        while (b != 0) {
-            
-            int remainder = a % b;
-            a = b;
-            b = remainder;
-        }
-        
-        return a; 
-        
-    }
-    
-    // 최소 공배수 메서드
-    public int getLCM(int a,int b){
-        return a * b / getGCD(a,b);
-    }
-    
     public int solution(int[] arr) {
-        
-        int lcm = arr[0];
-        
-        for (int i = 1; i < arr.length; i++){
-            lcm = getLCM(lcm,arr[i]);
+        int answer = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            answer = lcm(answer, arr[i]);
         }
-        
-        
-        return lcm;
+
+        return answer;
+    }
+
+    // 최대공약수(GCD) 구하기 - 유클리드 호제법
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    // 최소공배수(LCM) 구하기
+    private int lcm(int a, int b) {
+        return a * b / gcd(a, b);
     }
 }
