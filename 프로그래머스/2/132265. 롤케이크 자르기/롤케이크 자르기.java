111e984ff7089e19088c711ec8f038f7ca066e76
Minseong <kcm1042@naver.com>
2025-11-03 22:59:33 +0900

    [level 2] Title: 롤케이크 자르기, Time: 97.09 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -2,35 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] topping) {
-        int answer = 0;
         int n = topping.length;
-
-        // 철수와 동생이 가진 토핑의 종류 개수를 저장하는 배열
-        int[] leftUniqueCount = new int[n];  
-        int[] rightUniqueCount = new int[n];
-
+        
+        int[] leftCount = new int[n];
+        int[] rightCount = new int[n];
+        
+        // 왼쪽 토핑 종류 수 계산
         Set<Integer> leftSet = new HashSet<>();
-        Set<Integer> rightSet = new HashSet<>();
-
-        // 왼쪽에서 오른쪽으로 철수의 토핑 종류 개수 계산
         for (int i = 0; i < n; i++) {
             leftSet.add(topping[i]);
-            leftUniqueCount[i] = leftSet.size();
+            leftCount[i] = leftSet.size();
         }
-
-        // 오른쪽에서 왼쪽으로 동생의 토핑 종류 개수 계산
-        for (int i = n - 1; i > 0; i--) {
+        
+        // 오른쪽 토핑 종류 수 계산
+        Set<Integer> rightSet = new HashSet<>();
+        for (int i = n - 1; i >= 0; i--) {
             rightSet.add(topping[i]);
-            rightUniqueCount[i] = rightSet.size();
+            rightCount[i] = rightSet.size();
         }
-
-        // 공평하게 나눌 수 있는 지점 찾기
+        
+        // 공평하게 나누는 지점 세기
+        int answer = 0;
         for (int i = 0; i < n - 1; i++) {
-            if (leftUniqueCount[i] == rightUniqueCount[i + 1]) {
+            if (leftCount[i] == rightCount[i + 1]) {
                 answer++;
             }
         }
-
+        
         return answer;
     }
 }
