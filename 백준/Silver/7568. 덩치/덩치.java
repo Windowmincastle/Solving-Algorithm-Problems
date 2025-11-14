fa67b47e6eb4bfa1586400f5461ed1feee8305fc
Minseong <kcm1042@naver.com>
2025-11-14 09:07:18 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 15824 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,40 +1,61 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+class Person {
+    
+    int weight;
+    int height;
+    
+    Person(int weight, int height){
+        
+        this.weight = weight;
+        this.height = height;
+        
+    }
+    
+}
+
+public class Main {
+    
+    static void compare(List<Person> list, int N){
+       
+        List<Integer> answer = new ArrayList<>();
+        for(int i=0; i<N; i++){
+            int num = 0;
+            for(int j=0; j<N; j++){
+                if (i==j) continue;
+                if (list.get(j).weight > list.get(i).weight &&
+                    list.get(j).height > list.get(i).height) {
+
+                    num = num + 1;
+                }
+                
+            }
+            answer.add(num);
+        }
+        
+        for (int rank : answer){
+            System.out.print((rank+1)+" ");
+        }
+    }
     
     public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Person> list = new ArrayList<>();
         
-        int n = Integer.parseInt(br.readLine());
-        int m = 2;
-        int[][] ary = new int[n][m];
-
-        for (int i=0; i<n; i++){
-
-            String[] input = br.readLine().split(" ");
+        for (int i=0; i<N; i++){
             
-            ary[i][0] = Integer.parseInt(input[0]);
-            ary[i][1] = Integer.parseInt(input[1]);
-        
+            String[] wh = br.readLine().split(" ");
+            int w = Integer.parseInt(wh[0]);
+            int h = Integer.parseInt(wh[1]);
+
+            list.add(new Person(w,h)); // 보통은 인스턴스명을 선언하는데 왜 안할까?
         }
         
-        StringBuilder sb = new StringBuilder();
+        compare(list, N);
         
-        for (int j=0; j<n; j++){
-
-            int count = 1;    
-            
-            for (int k=0; k<n; k++){
-                
-                if( ary[j][0] < ary[k][0] && ary[j][1] < ary[k][1] ){
-                    count++;
-                }
-            }
-            sb.append(count).append(" ");
-        }
-        
-        System.out.println(sb);
     }
+    
 }
