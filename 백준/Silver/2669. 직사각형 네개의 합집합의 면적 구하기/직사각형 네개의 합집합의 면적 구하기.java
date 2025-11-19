4860deaa289cc123da1dd1ee09a91f04e1329767
Minseong <kcm1042@naver.com>
2025-11-19 17:28:35 +0900

    [Silver V] Title: 직사각형 네개의 합집합의 면적 구하기, Time: 168 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
+++ 백준/Silver/2669. 직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기.java
@@ -1,42 +1,36 @@
-import java.io.*;
-import java.util.*;
-public class Main{
-    
-    public static void main(String[] args) throws Exception {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        Scanner sc = new Scanner(System.in);
         
-        int size = 101;
-        boolean map[][] = new boolean[size][size];
+        int[][] board = new int[101][101]; // 좌표 1~100 사용
         
-        for(int i=0; i<4; i++){
+        for (int i=0; i<4; i++){
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
             
-            st = new StringTokenizer(br.readLine());
-            
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            
-            for (int x=x1; x<x2; x++){
+            // 직사각형 영역을 1로 채우기
+            for(int x= x1; x< x2; x++){
                 for(int y=y1; y<y2; y++){
-                    map[x][y] = true;
-                }
-            }
-            
-        }
-        
-        int count = 0;
-        for (int i=0; i<101; i++){
-            for (int j=0; j<101; j++){
-                if (map[i][j]){
-                    count++;
+                    board[x][y] = 1;
                 }
             }
         }
         
-        System.out.println(count);
+        int area = 0;
+        
+        for(int i=1; i<=100; i++){
+            for(int j=1; j<=100; j++){
+                if(board[i][j] == 1){
+                    area++;
+                }
+            }
+        }
+        
+        System.out.println(area);
     }
-    
 }
