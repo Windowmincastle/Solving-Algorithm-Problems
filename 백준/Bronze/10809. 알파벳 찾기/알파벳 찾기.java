a6b0d95f96daa9c1d80b088d196efa2e0b902679
Minseong <kcm1042@naver.com>
2025-11-27 09:19:51 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 100 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
-
+        
+        char[] words = {
+            'a','b','c','d','e','f','g',
+            'h','i','j','k','l','m','n',
+            'o','p','q','r','s','t','u',
+            'v','w','x','y','z'
+        };
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String input = br.readLine();
-        StringBuilder sb = new StringBuilder();
-        char[] ary = {'a','b','c','d','e','f','g','h','i','j',
-                      'k','l','m','n','o','p','q','r','s','t',
-                      'u','v','w','x','y','z'};
-
-        for (int i = 0; i < ary.length; i++) {
-
-            char target = ary[i];
-
-            // 괄호 수정 완료
-            if (input.contains(String.valueOf(target))) {
-                sb.append(input.indexOf(target)).append(" ");
-            } else {
-                sb.append("-1").append(" ");
-            }
-
+        String input      = br.readLine();
+        StringBuilder sb  = new StringBuilder();
+        
+        for(char c : words ){
+            sb.append(input.indexOf(c)).append(" ");            
         }
-        System.out.println(sb.toString());
-    }
+        
+        System.out.println(sb);
+    }    
 }
