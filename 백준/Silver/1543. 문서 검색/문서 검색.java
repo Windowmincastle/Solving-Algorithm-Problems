49a3a8fc3a53c01a7e7473ffcbea2725d215b281
Minseong <kcm1042@naver.com>
2025-11-27 23:04:25 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,26 +1,26 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    
     public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         String document = br.readLine();
-        String word     = br.readLine();
-
+        String word = br.readLine();
+        
         int count = 0;
-        int docLen = document.length();
-        int wLen   = word.length();
-
-        // i 를 직접 조정하는 for 문으로 깔끔하게 처리
-        for (int i = 0; i <= docLen - wLen; ) {
-            if (document.startsWith(word, i)) {
-                count++;
-                i += wLen;           // 일치 시엔 단어 길이만큼 건너뛰기
-            } else {
-                i++;                 // 불일치 시엔 한 칸 이동
-            }
+        int idx   = 0;
+        
+        while (true) {
+            int found = document.indexOf(word,idx);
+            if (found == -1) break;
+            
+            count++;
+            idx = found + word.length();
         }
-
         System.out.println(count);
     }
+    
 }
