c275e8f0bfe01c757971b4730b26b9d29efeba06
Minseong <kcm1042@naver.com>
2025-11-29 21:40:38 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,42 +1,40 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 빠른 입력을 위한 BufferedReader 사용
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
 
-        int n = Integer.parseInt(br.readLine()); // 단어 개수
-        int count = 0; // 그룹 단어 개수
+        for (int i=0; i<n; i++){
 
-        for (int i = 0; i < n; i++) {
-            String word = br.readLine();
-            if (isGroupWord(word)) {
-                count++;
-            }
-        }
-
-        System.out.println(count);
-    }
-
-    public static boolean isGroupWord(String word) {
-        boolean[] visited = new boolean[26]; // 알파벳 등장 여부 저장
-        char prev = 0;
-
-        for (int i = 0; i < word.length(); i++) {
-            char curr = word.charAt(i);
-
-            if (curr != prev) {
-                if (visited[curr - 'a']) {
-                    return false; // 이미 등장했던 문자가 다시 나오면 그룹 단어 아님
+            Set<Character> set = new HashSet<>();
+            String groupWord = br.readLine();
+            boolean isGroup = true;
+            
+            char prevWord = groupWord.charAt(0);
+            set.add(prevWord);
+            
+            for (int j=1; j<groupWord.length(); j++){
+                
+                char currentWord = groupWord.charAt(j);
+                
+                if (currentWord == prevWord) continue;    
+                    
+                if (set.contains(currentWord)) {
+                    isGroup = false;
+                    break;
                 }
-                visited[curr - 'a'] = true;
-                prev = curr;
+                
+                set.add(currentWord);
+                prevWord = currentWord;
             }
-            // 같을 경우는 연속된 문자이므로 처리 필요 없음
+            
+            if (isGroup) answer++;
         }
-
-        return true;
+        
+        System.out.println(answer);
     }
 }
