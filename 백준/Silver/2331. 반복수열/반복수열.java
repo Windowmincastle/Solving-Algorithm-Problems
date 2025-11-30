3911e5f77bb2ab1b00b4c5b8dc37454614308e7e
Minseong <kcm1042@naver.com>
2025-11-30 22:50:48 +0900

    [Silver IV] Title: 반복수열, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.java
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -1,42 +1,47 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int P = sc.nextInt();
-        sc.close();
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        // 숫자가 처음 등장한 순서를 저장하는 Map
+        Map<Integer, Integer> map = new HashMap<>();
         
-        // 0~9의 P제곱을 미리 계산
-        int[] pow = new int[10];
-        for (int d = 0; d <= 9; d++) {
-            pow[d] = (int) Math.pow(d, P);
-        }
-        
-        // 수 → 등장 순서를 저장하는 맵
-        Map<Integer, Integer> order = new HashMap<>();
-        int cur = A;
-        int idx = 1;
-        order.put(cur, idx);
-        
+        int index = 0; // 등장 순서
+        int current = A;
+
         while (true) {
-            // cur의 각 자리 숫자 P제곱합 계산
-            int next = 0;
-            int tmp = cur;
-            while (tmp > 0) {
-                next += pow[tmp % 10];
-                tmp /= 10;
-            }
-            
-            idx++;
-            if (order.containsKey(next)) {
-                // next가 처음 등장한 순서에서 1을 빼면, 반복 부분이 시작되기 전까지 남는 수의 개수
-                System.out.println(order.get(next) - 1);
+
+            // 이미 등장한 적 있는 숫자 → 반복 시작
+            if (map.containsKey(current)) {
+                System.out.println(map.get(current));
                 break;
-            } else {
-                order.put(next, idx);
-                cur = next;
             }
+
+            // 숫자가 처음 등장했으므로 등장 위치 저장
+            map.put(current, index++);
+            
+            // 다음 수 생성
+            current = nextValue(current, P);
         }
+    }
+
+    // D[n] 계산 함수
+    private static int nextValue(int num, int P) {
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, P);
+            num /= 10;
+        }
+
+        return sum;
     }
 }
