d971582d6065cadfedd32e99a19d50e1314b1ae4
Minseong <kcm1042@naver.com>
2025-12-16 23:25:43 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 176 ms, Memory: 16508 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -1,40 +1,46 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder result = new StringBuilder();
+        String s = br.readLine();
+        
+        StringBuilder result = new StringBuilder(); // 최종 결과 저장
+        StringBuilder word = new StringBuilder();   // 뒤집을 단어 임시 저장
+        boolean inTag = false; // 태그 안인지 확인하는 플래그
 
-        String input = br.readLine();
-        StringBuilder temp = new StringBuilder();
-        boolean inTag = false; // 태그 안에 있는지 여부
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
 
-        for (char c : input.toCharArray()) {
-            if (c == '<') {
-                // 이전에 쌓인 단어를 뒤집어서 결과에 추가
-                result.append(temp.reverse());
-                temp.setLength(0); // temp 초기화
-                inTag = true; // 태그 시작
-                result.append(c); // 태그 여는 문자 추가
-            } else if (c == '>') {
-                inTag = false; // 태그 종료
-                result.append(c); // 태그 닫는 문자 추가
+            if (ch == '<') {
+                // 태그 시작 전까지 쌓인 단어가 있다면 뒤집어서 추가
+                result.append(word.reverse().toString());
+                word.setLength(0); // 단어 초기화
+                inTag = true;
+                result.append(ch);
+            } else if (ch == '>') {
+                inTag = false;
+                result.append(ch);
             } else if (inTag) {
-                result.append(c); // 태그 내부는 그대로 추가
+                // 태그 내부라면 그대로 추가
+                result.append(ch);
             } else {
-                if (c == ' ') {
-                    // 단어가 끝나면 뒤집어서 결과에 추가
-                    result.append(temp.reverse());
-                    temp.setLength(0); // temp 초기화
-                    result.append(c); // 공백 추가
+                // 태그 외부일 때
+                if (ch == ' ') {
+                    // 공백을 만나면 그동안 쌓인 단어 뒤집어서 추가
+                    result.append(word.reverse().toString());
+                    result.append(ch);
+                    word.setLength(0);
                 } else {
-                    temp.append(c); // 단어 저장
+                    // 단어 구성 문자라면 임시 저장
+                    word.append(ch);
                 }
             }
         }
-
-        // 마지막 단어 처리
-        result.append(temp.reverse());
+        
+        // 마지막에 남은 단어가 있다면 뒤집어서 추가
+        result.append(word.reverse().toString());
 
         System.out.println(result.toString());
     }
