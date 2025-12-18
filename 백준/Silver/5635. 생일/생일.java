b74dce3ccfa970a437762b2a65ebb6b39e86bc2d
Minseong <kcm1042@naver.com>
2025-12-18 09:44:14 +0900

    [Silver V] Title: 생일, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.java
+++ 백준/Silver/5635. 생일/생일.java
@@ -1,56 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    
+public class Main {
+
     static class Student {
-        
         String name;
-        int day;
-        int month;
-        int year;
-        
-        public Student(String name,int day,int month,int year){
+        int day, month, year;
+
+        Student(String name, int day, int month, int year) {
             this.name = name;
             this.day = day;
             this.month = month;
             this.year = year;
         }
-        
     }
-    
+
     public static void main(String[] args) throws Exception {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
-        List<Student> students = new ArrayList<>();
-        
-        for(int i=0; i<n; i++){
-            
+
+        List<Student> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
             int day = Integer.parseInt(st.nextToken());
             int month = Integer.parseInt(st.nextToken());
             int year = Integer.parseInt(st.nextToken());
-            
-            students.add(new Student(name,day,month,year));
-            
+
+            list.add(new Student(name, day, month, year));
         }
-        
-        students.sort((s1, s2) -> {
-            if (s1.year != s2.year) {
-                return s1.year - s2.year; // 연도 기준 정렬
-            }
-            if (s1.month != s2.month) {
-                return s1.month - s2.month; // 월 기준 정렬
-            }
-            return s1.day - s2.day; // 일 기준 정렬
+
+        Collections.sort(list, (a, b) -> {
+            if (a.year != b.year) return a.year - b.year;
+            if (a.month != b.month) return a.month - b.month;
+            return a.day - b.day;
         });
-        
-        System.out.println(students.get(n-1).name);
-        System.out.println(students.get(0).name);
-        
+
+
+        // 가장 나이가 적은 사람 (맨 뒤)
+        System.out.println(list.get(list.size() - 1).name);
+        // 가장 나이가 많은 사람 (맨 앞)
+        System.out.println(list.get(0).name);
     }
-    
 }
