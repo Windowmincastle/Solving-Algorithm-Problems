b2e92301b5cbbd6d74bf8b61eb67647b55b87648
Minseong <kcm1042@naver.com>
2025-12-22 11:34:45 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 9.19 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -1,11 +1,17 @@
+import java.io.*;
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
-        
+        // 10을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록
+        // n보다 큰 수로 나누는건 의미가 없다.
+        // 가장 작은 자연수를 갱신한다.
         int answer = Integer.MAX_VALUE;
-        for (int x=1000000; x>=2; x--){
+        
+        for (int i=2; i<=n; i++) {
             
-            if (n % x == 1){
-                answer = Math.min(x,answer);
+            if ( n % i == 1 ) {
+                answer = Math.min(i,answer);
             }
         }
         
