b6d6597766a88389a6d83cc1de75d0d7b8660c94
Minseong <kcm1042@naver.com>
2025-12-23 11:01:39 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 10.98 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -1,31 +1,20 @@
 class Solution {
-    
-    public static long getStartEndSum(int start, int end){
-        
-        if( start == end ){
-            return start;
-        }
-        
-        int temp = 0;
-        
-        if (start > end ){
-            temp = start;
-            start = end;
-            end = temp;
-        }
-        
-        
-        long sum = 0;
-        
-        for (int i=start; i<=end; i++){
-            sum += i;
-        }
-        
-        return sum;
-    }
-    
     public long solution(int a, int b) {
-        long answer = getStartEndSum(a,b);
+        long answer = 0;
+        
+        if (a == b) return a;
+        
+        int swap = 0;
+        if (a > b) {
+            swap = b;
+            b = a;
+            a = swap;
+        }
+        
+        for (int i=a; i<=b; i++) {
+            answer += i;
+        }
+        
         return answer;
     }
 }
