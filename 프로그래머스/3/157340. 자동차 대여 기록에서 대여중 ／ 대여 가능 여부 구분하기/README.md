db666e1eaa726f7b8ad830aca9e13d457539c8da
Minseong <kcm1042@naver.com>
2025-12-30 14:04:38 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/README.md
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기 - 157340 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157340) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/157340?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2025년 09월 26일 17:42:35
+2025년 12월 30일 14:04:37
 
 ### 문제 설명
 
--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,12 +1,17 @@
 SELECT
-    CAR_ID,  -- 자동차 ID별로 결과를 출력
-    CASE 
-        -- 기준 날짜(2022-10-16)에 대여 중인 기록이 하나라도 있으면 '대여중'
-        WHEN SUM(START_DATE <= '2022-10-16' AND END_DATE >= '2022-10-16') > 0
+    CAR_ID,
+    CASE
+        WHEN MAX(
+            CASE
+                WHEN START_DATE <= '2022-10-16'
+                 AND END_DATE   >= '2022-10-16'
+                THEN 1
+                ELSE 0
+            END
+        ) = 1
         THEN '대여중'
-        -- 기준 날짜에 대여 중인 기록이 하나도 없으면 '대여 가능'
         ELSE '대여 가능'
     END AS AVAILABILITY
 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-GROUP BY CAR_ID  -- 자동차 ID별로 그룹화하여 하나의 결과만 남김
-ORDER BY CAR_ID DESC;  -- 자동차 ID 내림차순으로 정렬
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC;
