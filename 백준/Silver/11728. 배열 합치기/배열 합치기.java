0eb43fcf01e5c6d1ee55fe652a5bffaabc5c0eb3
Minseong <kcm1042@naver.com>
2026-01-02 22:11:27 +0900

    [Silver V] Title: 배열 합치기, Time: 1268 ms, Memory: 351632 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,55 +1,57 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 입력을 위한 BufferedReader
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 출력 성능 향상을 위한 StringBuilder
-        StringBuilder sb = new StringBuilder();
-
-        // 첫 줄에서 N과 M 입력 받기
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        // 두 배열 A, B 생성
-        int[] A = new int[N];
-        int[] B = new int[M];
-
-        // A 배열 입력
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
+        int aLen = Integer.parseInt(input[0]);
+        int bLen = Integer.parseInt(input[1]);
+        
+        int[] A = new int[aLen];
+        String[] temp = br.readLine().split(" ");
+        
+        for (int i=0; i<aLen; i++) {
+            A[i] = Integer.parseInt(temp[i]);
         }
-
-        // B 배열 입력
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            B[i] = Integer.parseInt(st.nextToken());
+        
+        int[] B = new int[bLen];
+        String[] temp2 = br.readLine().split(" ");
+        for (int j=0; j<bLen; j++) {
+            B[j] = Integer.parseInt(temp2[j]);
         }
-
-        // 두 포인터를 이용해 정렬된 배열 합치기
-        int i = 0, j = 0;
-        while (i < N && j < M) {
-            if (A[i] <= B[j]) {
-                sb.append(A[i++]).append(" ");
+        
+        List<Integer> list = new ArrayList<>();
+        
+        int pointer1 = 0, pointer2 = 0;
+        while (pointer1 < aLen && pointer2 < bLen ) {
+            if (A[pointer1] < B[pointer2] ) {
+                list.add(A[pointer1]);
+                pointer1++;
             } else {
-                sb.append(B[j++]).append(" ");
+                list.add(B[pointer2]);
+                pointer2++;
             }
         }
-
-        // 남은 원소 처리
-        while (i < N) {
-            sb.append(A[i++]).append(" ");
+        
+        while(pointer1<aLen) {
+            list.add(A[pointer1]);
+            pointer1++;
         }
-        while (j < M) {
-            sb.append(B[j++]).append(" ");
+        
+        while(pointer2<bLen){
+          list.add(B[pointer2]);
+          pointer2++;
+        } 
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int num : list) {
+            sb.append(num).append(" ");
         }
-
-        // 결과 출력
-        System.out.println(sb);
+        
+        System.out.println(sb.toString());
     }
 }
