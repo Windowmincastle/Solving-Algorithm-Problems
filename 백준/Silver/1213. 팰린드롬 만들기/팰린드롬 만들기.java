ea07b386a8e906da25bf3e2379ceeade47aa77f6
Minseong <kcm1042@naver.com>
2026-01-04 21:41:41 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,54 +1,52 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        sc.close();
-        
-        // 1. 문자 빈도수 세기
-        Map<Character, Integer> charCount = new HashMap<>();
-        for (char c : input.toCharArray()) {
-            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+
+        int[] cnt = new int[26];
+        for (char c : name.toCharArray()) {
+            cnt[c - 'A']++;
         }
-        
-        // 2. 홀수 개수의 문자 확인
+
         int oddCount = 0;
-        char oddChar = 0;
-        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
-            if (entry.getValue() % 2 != 0) {
+        int oddIndex = -1;
+
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] % 2 == 1) {
                 oddCount++;
-                oddChar = entry.getKey();
+                oddIndex = i;
             }
         }
-        
-        // 홀수 개수의 문자가 두 개 이상이면 회문을 만들 수 없음
+
+        // 팰린드롬 불가능
         if (oddCount > 1) {
             System.out.println("I'm Sorry Hansoo");
             return;
         }
-        
-        // 3. 반쪽 문자열 만들기
-        TreeSet<Character> sortedChars = new TreeSet<>(charCount.keySet());
-        StringBuilder half = new StringBuilder();
-        for (char c : sortedChars) {
-            int count = charCount.get(c);
-            for (int i = 0; i < count / 2; i++) {
-                half.append(c);
+
+        StringBuilder left = new StringBuilder();
+
+        // 사전순으로 왼쪽 절반 생성
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < cnt[i] / 2; j++) {
+                left.append((char) ('A' + i));
             }
         }
-        
-        // 4. 회문 구성
-        String halfStr = half.toString();
-        String reverseHalfStr = half.reverse().toString();
-        
-        if (oddCount == 1) {
-            System.out.println(halfStr + oddChar + reverseHalfStr);
-        } else {
-            System.out.println(halfStr + reverseHalfStr);
+
+        StringBuilder result = new StringBuilder();
+        result.append(left);
+
+        // 가운데 문자 (있는 경우)
+        if (oddIndex != -1) {
+            result.append((char) ('A' + oddIndex));
         }
+
+        // 오른쪽 절반
+        result.append(left.reverse());
+
+        System.out.println(result.toString());
     }
 }
