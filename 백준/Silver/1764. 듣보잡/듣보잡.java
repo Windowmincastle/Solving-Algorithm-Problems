84bd15ac6d6d7df3b5cba55ed517cf31f536251f
Minseong <kcm1042@naver.com>
2026-01-10 22:05:04 +0900

    [Silver IV] Title: 듣보잡, Time: 272 ms, Memory: 24688 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,40 +1,52 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 입력을 빠르게 받기 위한 BufferedReader
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        // 듣도 못한 사람 수(N)과 보도 못한 사람 수(M) 입력
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        // 듣도 못한 사람들을 저장할 HashSet
-        Set<String> unheard = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            unheard.add(br.readLine());
+        String[] input = br.readLine().split(" ");
+        
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        
+        String[] emeeh = new String[n];
+        String[] qheh = new String[m];
+        
+        for (int i=0; i<n; i++) {
+            emeeh[i] = br.readLine();
         }
-
-        // 교집합을 저장할 ArrayList
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            String name = br.readLine();
-            if (unheard.contains(name)) {
-                result.add(name);
+        
+        for (int j=0; j<m; j++) {
+            qheh[j] = br.readLine();
+        }
+        
+        Arrays.sort(emeeh);
+        Arrays.sort(qheh);
+        
+        int p1 = 0;
+        int p2 = 0;
+        List<String> lst = new ArrayList<>();
+        
+        while(p1 < n && p2 < m) {
+            
+            if (emeeh[p1].equals(qheh[p2])){
+                lst.add(emeeh[p1]);
+                p1++;
+                p2++;
+            } else if ( emeeh[p1].compareTo(qheh[p2]) < 0) {
+                p1++;
+            } else if ( emeeh[p1].compareTo(qheh[p2]) > 0) {
+                p2++;
             }
+            
         }
-
-        // 결과를 사전순으로 정렬
-        Collections.sort(result);
-
-        // 출력
-        StringBuilder sb = new StringBuilder();
-        sb.append(result.size()).append("\n");
-        for (String name : result) {
-            sb.append(name).append("\n");
+        
+        System.out.println(lst.size());
+        for (String str : lst) {
+            System.out.println(str);
         }
-        System.out.print(sb);
     }
+    
 }
