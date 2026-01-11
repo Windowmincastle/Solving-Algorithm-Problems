9bc6391d14d58aaa0439eb5e89156ff6378eecab
Minseong <kcm1042@naver.com>
2026-01-11 23:45:52 +0900

    [Silver III] Title: 블로그, Time: 312 ms, Memory: 36088 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,52 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-
-
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String[] firstLine = br.readLine().split(" ");
-        int N = Integer.parseInt(firstLine[0]);
-        int X = Integer.parseInt(firstLine[1]);
-        String[] input = br.readLine().split(" ");
-        int[] ary = new int[N];
-        for (int i = 0; i < N; i++) {
-            ary[i] = Integer.parseInt(input[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        
+        int[] days = new int[n];
+        st = new StringTokenizer(br.readLine());
+        
+        for (int i=0; i<n; i++) {
+             days[i] = Integer.parseInt(st.nextToken());   
         }
-
-        //초기 x일동안의 합을 계산
-        int curSum = 0;
-        for (int i = 0; i < X; i++) {
-            curSum += ary[i];
+        
+        int answer = 0, sum = 0, count =0;
+        for (int j=0; j<x; j++) {
+            sum+= days[j];
         }
-
-        int maxValue = curSum;
-        int cnt = 1;
-
-        for (int i = X; i < N; i++) {
-
-            curSum = curSum + ary[i] - ary[i - X];
-
-            if (curSum > maxValue) {
-                cnt = 1;
-                maxValue = curSum;
-            } else if (curSum == maxValue) {
-                cnt++;
+        
+        answer = Math.max(answer,sum);
+        count = 1;
+        
+        for (int k=x; k<n; k++) {
+            sum -= days[k-x];
+            sum += days[k];
+            
+            if ( sum > answer ) {
+                answer = Math.max(answer,sum);
+                count = 1;
+            } else if ( sum == answer){
+                count++;
             }
+            
         }
-
-        if (maxValue == 0) {
+        
+        if ( answer == 0 ){
             System.out.println("SAD");
         } else {
-            sb.append(maxValue).append("\n");
-            sb.append(cnt);
-            System.out.println(sb);
+            System.out.println(answer);
+            System.out.println(count);
         }
-
+        
     }
+    
 }
-
