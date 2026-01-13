59aa3f952559aba7a73fcee05773a17a1c4522ed
Minseong <kcm1042@naver.com>
2026-01-13 13:23:17 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -2,25 +2,22 @@ import java.io.*;
 
 public class Main {
     
-    public static int factorial(int n){
+    static int factorial(int n) {
         
-        if (n==0){
-            
+        if(n <= 1) {
             return 1;
         }
         
         return n * factorial(n-1);
-        
     }
     
-    public static void main(String[] args ) throws Exception {
+    public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
         
-        System.out.println(factorial(n));
-        
+        int answer = factorial(n);
+        System.out.println(answer);
     }
     
 }
