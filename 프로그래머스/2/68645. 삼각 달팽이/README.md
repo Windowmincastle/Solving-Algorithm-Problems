67db6f15d17d6dc5b64a80cf7ff9add65baae795
Minseong <kcm1042@naver.com>
2026-01-18 22:52:58 +0900

    [level 2] Title: 삼각 달팽이, Time: 15.73 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/README.md
+++ 프로그래머스/2/68645. 삼각 달팽이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 삼각 달팽이 - 68645 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68645?language=java) 
 
 ### 성능 요약
 
-메모리: 131 MB, 시간: 17.08 ms
+메모리: 144 MB, 시간: 15.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 17일 20:03:05
+2026년 01월 18일 22:52:52
 
 ### 문제 설명
 
--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,39 +1,52 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
     public int[] solution(int n) {
-        int[][] arr = new int[n][n];
-        
-        int num = 1;
-        int r = -1, c = 0;
-        
-        for (int i = 0; i < n; i++) { 
-            for (int j = i; j < n; j++) {
-                // 방향 결정
-                if (i % 3 == 0) {
-                    // 아래로 이동
-                    r++;
-                } else if (i % 3 == 1) {
-                    // 오른쪽으로 이동
-                    c++;
-                } else {
-                    // 왼쪽 위로 이동
-                    r--;
-                    c--;
-                }
-                arr[r][c] = num++;
+        // n x n 배열을 사용해 삼각형 영역에만 값을 채운다.
+        int[][] triangle = new int[n][n];
+        int x = 0, y = 0;          // 현재 위치 (행, 열)
+        int num = 1;               // 채워 넣을 숫자
+        int size = n;              // 현재 한 사이클(아래-오른쪽-대각위)에서 이동할 길이
+
+        while (size > 0) {
+            // 아래로 size번 이동하며 채운다.
+            for (int i = 0; i < size; i++) {
+                triangle[x++][y] = num++;
             }
+            x--; y++; size--;      // 위치 보정 및 다음 단계 길이 감소
+            if (size <= 0) break;
+
+            // 오른쪽으로 size번 이동하며 채운다.
+            for (int i = 0; i < size; i++) {
+                triangle[x][y++] = num++;
+            }
+            y -= 2; x--; size--;   // 위치 보정 및 다음 단계 길이 감소
+            if (size <= 0) break;
+
+            // 위-왼쪽(대각선)으로 size번 이동하며 채운다.
+            for (int i = 0; i < size; i++) {
+                triangle[x--][y--] = num++;
+            }
+            x += 2; y++; size--;   // 위치 보정
         }
-        
-        // 결과 배열 크기
-        int size = n * (n + 1) / 2;
-        int[] answer = new int[size];
-        
+
+        // 삼각형 영역(각 행의 0..i 열) 순서대로 1차원 배열로 반환
+        int total = n * (n + 1) / 2;
+        int[] answer = new int[total];
         int idx = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                answer[idx++] = arr[i][j];
+                answer[idx++] = triangle[i][j];
             }
         }
-        
         return answer;
+    }
+
+    // 테스트용 main (예시 출력)
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(4))); // [1,2,9,3,10,8,4,5,6,7]
+        System.out.println(Arrays.toString(s.solution(5))); // [1,2,12,3,13,11,4,14,15,10,5,6,7,8,9]
+        System.out.println(Arrays.toString(s.solution(6))); // [1,2,15,3,16,14,4,17,21,13,5,18,19,20,12,6,7,8,9,10,11]
     }
 }
