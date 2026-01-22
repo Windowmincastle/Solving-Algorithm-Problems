702b6d0ee7a37f9c1833c00b0669056211d149de
Minseong <kcm1042@naver.com>
2026-01-22 16:10:45 +0900

    [Silver IV] Title: 국영수, Time: 628 ms, Memory: 60332 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.java
+++ 백준/Silver/10825. 국영수/국영수.java
@@ -2,50 +2,67 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Student {
-        String name;
-        int korean, english, math;
-
-        public Student(String name, int korean, int english, int math) {
-            this.name = name;
-            this.korean = korean;
-            this.english = english;
-            this.math = math;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    
+    public static void main(String[] args) throws Exception {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
+        
+        List<Student> list = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        List<Student> students = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            String name = input[0];
-            int kor = Integer.parseInt(input[1]);
-            int eng = Integer.parseInt(input[2]);
-            int math = Integer.parseInt(input[3]);
-            students.add(new Student(name, kor, eng, math));
+            String name = st.nextToken();
+            int k = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            
+            list.add(new Student(name,k,e,m));
         }
-
-        students.sort((a, b) -> {
-            if (a.korean != b.korean) {
-                return b.korean - a.korean; // 국어 내림차순
-            } else if (a.english != b.english) {
-                return a.english - b.english; // 영어 오름차순
-            } else if (a.math != b.math) {
-                return b.math - a.math; // 수학 내림차순
-            } else {
-                return a.name.compareTo(b.name); // 이름 사전순
-            }
-        });
-
-        for (Student s : students) {
-            sb.append(s.name).append('\n');
+        
+        // Comparable 기준을 쓰고싶다면 Comparator.naturalOrder()를 인자로 전달
+        list.sort(Comparator.naturalOrder());
+        
+        StringBuilder sb = new StringBuilder();
+        for (Student stu : list) {
+            sb.append(stu.name).append("\n");
         }
+        
+        System.out.println(sb.toString());
+    }
+}
 
-        System.out.print(sb);
+class Student implements Comparable<Student> {
+    
+    String name;
+    int K;
+    int E;
+    int M;
+    
+    Student(String name, int K, int E, int M) {
+        this.name = name;
+        this.K = K;
+        this.E = E;
+        this.M = M;
+    }
+    
+    @Override
+    public int compareTo(Student o2) {
+        
+        // 국어 점수 비교, 내림차순
+        if (this.K != o2.K) {
+            return o2.K - this.K;
+        }
+        
+        // 국어 점수가 같다면 영어 점수를 오름차순으로 비교한다.
+        if (this.E != o2.E) {
+            return this.E - o2.E;   
+        }
+        // 국어 점수도 같고, 영어점수도 같으면 수학 점수 내림차순
+        if (this.M != o2.M) return o2.M - this.M;
+        
+        // 모든 점수가 같으면 이름이 사전 순으로 증가하는 순서로 
+        return this.name.compareTo(o2.name);
     }
 }
