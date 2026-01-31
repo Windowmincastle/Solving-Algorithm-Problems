c2fd95a1e0dcb1360e8f986fec0b13124390df25
Minseong <kcm1042@naver.com>
2026-01-31 23:28:54 +0900

    [Silver V] Title: 분수찾기, Time: 124 ms, Memory: 16116 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,38 +1,37 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-    public static void main(String[] args) throws Exception {
-    
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int X = Integer.parseInt(br.readLine());
-        
-        int diagonal = 0; // 현재 대각선 번호.
-        int sum = 0; // 대각선 까지의 누적합
-        
-        //X가 포함된 대각선 번호 찾기
+
+        int diagonal = 0;
+        int sum = 0;
+
+        // 1. X가 속한 대각선 찾기
         while (sum < X) {
             diagonal++;
             sum += diagonal;
         }
-        
-        // X가 diagonal번째 대각선의 몇 번째 위치인지 계산하기.
-        int position = X - ( sum - diagonal);
-        
-        // 대각선의 홀수/짝수에 따라 분수 계산
+
+        // 2. 대각선 내 위치
+        int index = X - (sum - diagonal);
+
         int numerator, denominator;
-        
-        if (diagonal % 2 == 0 ) {
-            //짝수 대각선
-            numerator = position; // 분자 증가
-            denominator = diagonal - position + 1; // 분모는 감소
+
+        // 3. 지그재그 방향 결정
+        if (diagonal % 2 == 0) {
+            // 짝수 대각선
+            numerator = index;
+            denominator = diagonal - index + 1;
         } else {
-            //홀수 대각선
-            numerator = diagonal - position + 1; // 분자는 감소
-            denominator = position; //분모는 증가.
+            // 홀수 대각선
+            numerator = diagonal - index + 1;
+            denominator = index;
         }
-        
+
         System.out.println(numerator + "/" + denominator);
     }
 }
