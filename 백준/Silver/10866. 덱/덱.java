4832e98395b25c436a4f46c3726b83869ce0e307
Minseong <kcm1042@naver.com>
2026-02-10 13:44:56 +0900

    [Silver IV] Title: 덱, Time: 284 ms, Memory: 19812 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -6,58 +6,71 @@ public class Main {
     public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> dq = new ArrayDeque<>();
+        String empty = "-1";
         int n = Integer.parseInt(br.readLine());
-        Deque<Integer> deque = new ArrayDeque<>();
         
-        for (int i=0; i<n; i++){
+        for (int i=0; i<n; i++) {
             
-            String[] input = br.readLine().split(" ");
-            if (input[0].equals("push_front")){
-                
-                deque.addFirst(Integer.parseInt(input[1]));
-                
-            } else if (input[0].equals("push_back")){
-                
-                deque.addLast(Integer.parseInt(input[1]));
+            String[] orders = br.readLine().split(" ");
             
-            } else if (input[0].equals("pop_front")){
-                
-                if (deque.size() == 0) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(deque.removeFirst());
+            switch(orders[0]) {
+                case "push_front": {
+                    int value = Integer.parseInt(orders[1]);
+                    dq.offerFirst(value);
                 }
-            
-            } else if (input[0].equals("size")) {
-                System.out.println(deque.size());
-            } else if (input[0].equals("empty")){
-                
-                if (deque.size() == 0 ){
-                    System.out.println(1);
-                } else {
-                    System.out.println(0);
+                    break;
+                case "push_back": {
+                    int value = Integer.parseInt(orders[1]);
+                    dq.offerLast(value);
                 }
-                
-            } else if (input[0].equals("front")){
-                if ( deque.size()==0) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(deque.peekFirst());
-                }
-            } else if (input[0].equals("back")){
-                                if ( deque.size()==0) {
-                    System.out.println(-1);
-                } else {
-                    System.out.println(deque.peekLast());
-                }
-            } else if (input[0].equals("pop_back")){
-                
-                if (deque.size() == 0){
-                    System.out.println(-1);
-                } else {
-                    System.out.println(deque.removeLast());
-                }
+                    break;
+                case "pop_front":
+                    if(!dq.isEmpty()) {
+                        System.out.println(dq.pollFirst());
+                    } else {
+                        System.out.println(empty);
+                    }
+                    break;
+                case "pop_back":
+                    if(!dq.isEmpty()) {
+                        System.out.println(dq.pollLast());
+                    } else {
+                        System.out.println(empty);
+                    }
+                    break;
+                case "size":
+                    System.out.println(dq.size());
+                    break;
+                case "empty":
+                    if (!dq.isEmpty()) {
+                        System.out.println("0");
+                    } else {
+                        System.out.println("1");
+                    }
+                    break;
+                case "front":
+                    if(!dq.isEmpty()) {
+                        System.out.println(dq.peekFirst());
+                    } else {
+                        System.out.println(empty);
+                    }
+                    break;
+                case "back":
+                    if(!dq.isEmpty()) {
+                        System.out.println(dq.peekLast());
+                    } else {
+                        System.out.println(empty);
+                    }
+                    break;
+                    
             }
+            
         }
+        
+        
+        
     }
+    
 }
