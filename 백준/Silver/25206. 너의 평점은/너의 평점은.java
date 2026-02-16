00ffb9001cbf023f6d4458a9f5d4e610e43d9bda
Minseong <kcm1042@naver.com>
2026-02-16 16:42:29 +0900

    [Silver V] Title: 너의 평점은, Time: 112 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,49 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    
-    public static void main(String[] args ) throws Exception {
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         
-        // 등급별 점수 매핑
-        Map<String,Double> grade = new HashMap<>();
-        
-        grade.put("A+", 4.5);
-        grade.put("A0",4.0);
-        grade.put("B+",3.5);
-        grade.put("B0",3.0);
-        grade.put("C+",2.5);
-        grade.put("C0",2.0);
-        grade.put("D+",1.5);
-        grade.put("D0",1.0);
-        grade.put("F",0.0);
+        // 등급 → 평점 매핑
+        Map<String, Double> gradeMap = new HashMap<>();
+        gradeMap.put("A+", 4.5);
+        gradeMap.put("A0", 4.0);
+        gradeMap.put("B+", 3.5);
+        gradeMap.put("B0", 3.0);
+        gradeMap.put("C+", 2.5);
+        gradeMap.put("C0", 2.0);
+        gradeMap.put("D+", 1.5);
+        gradeMap.put("D0", 1.0);
+        gradeMap.put("F", 0.0);
 
-        double totalScore = 0.0;
-        double totalCredit = 0.0;
-        
-        for (int i=0; i<20; i++){
+        double totalScore = 0.0;   // 학점 × 평점 합
+        double totalCredit = 0.0;  // 학점 총합
 
+        for (int i = 0; i < 20; i++) {
             String[] input = br.readLine().split(" ");
             
-            String subject = input[0];
             double credit = Double.parseDouble(input[1]);
-            String gr = input[2];
+            String grade = input[2];
 
-            if (gr.equals("P")) continue;
+            // P는 계산 제외
+            if (grade.equals("P")) continue;
 
-            totalScore += credit * grade.get(gr); // 학점 x 과목평점의 합
-            totalCredit += credit; // 학점의 총합
-
+            totalCredit += credit;
+            totalScore += credit * gradeMap.get(grade);
         }
-        
+
         double gpa = totalScore / totalCredit;
-        sb.append(String.format("%.6f",gpa));
-        System.out.println(sb);
-
-
+        System.out.printf("%.6f\n", gpa);
     }
-    
 }
