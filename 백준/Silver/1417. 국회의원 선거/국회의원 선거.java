fa43dd136a5c4c807d08f7eaa357de1dbc9dfcdf
Minseong <kcm1042@naver.com>
2026-02-17 23:33:57 +0900

    [Silver V] Title: 국회의원 선거, Time: 92 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.java
@@ -4,35 +4,36 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       
-        //각 줄에 하나의 값이 들어오는 구조라서 StringTokenizer를 사용할 필요가 없다.
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        // 후보가 1명인 경우
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
 
-        int n = Integer.parseInt(br.readLine());
         int dasom = Integer.parseInt(br.readLine());
         
+        // 최대 힙 (내림차순)
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        for(int i=1; i<n; i++){
-            pq.offer(Integer.parseInt(br.readLine()));
+        for (int i = 1; i < N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
         }
-        
-        int cnt = 0;
-        
-        while( !pq.isEmpty() && dasom <= pq.peek()) {
-            
-            int maxVotes = pq.poll();
-            pq.offer(maxVotes -1);
-            
-            dasom++;
-            cnt++;
-            
-        }
-        
-        System.out.println(cnt);
-        
 
+        int count = 0;
+
+        while (!pq.isEmpty() && pq.peek() >= dasom) {
+            int top = pq.poll();
+            top--;        // 한 명 매수
+            dasom++;      // 다솜 표 증가
+            count++;
+
+            pq.add(top);
+        }
+
+        System.out.println(count);
     }
-    
 }
