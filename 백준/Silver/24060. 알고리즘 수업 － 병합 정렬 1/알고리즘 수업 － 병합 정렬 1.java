ead58bb08097dbfef4ff5f63da5d02efb9e9dd90
Minseong <kcm1042@naver.com>
2026-02-22 15:06:51 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 580 ms, Memory: 75032 KB -BaekjoonHub

--- 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -2,10 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] A, tmp;
-    static int K, count = 0, result = -1;
 
-    public static void mergeSort(int p, int r) {
+    static int[] A, tmp;
+    static long K;
+    static long count = 0;
+    static int result = -1;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        K = Long.parseLong(st.nextToken());
+
+        A = new int[N];
+        tmp = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        mergeSort(0, N - 1);
+
+        System.out.println(result);
+    }
+
+    static void mergeSort(int p, int r) {
         if (p < r) {
             int q = (p + r) / 2;
             mergeSort(p, q);
@@ -14,40 +37,38 @@ public class Main {
         }
     }
 
-    public static void merge(int p, int q, int r) {
-        int i = p, j = q + 1, t = 0;
+    static void merge(int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        int t = 0;
 
         while (i <= q && j <= r) {
-            if (A[i] <= A[j]) tmp[t++] = A[i++];
-            else tmp[t++] = A[j++];
+            if (A[i] <= A[j]) {
+                tmp[t++] = A[i++];
+            } else {
+                tmp[t++] = A[j++];
+            }
         }
 
-        while (i <= q) tmp[t++] = A[i++];
-        while (j <= r) tmp[t++] = A[j++];
+        while (i <= q) {
+            tmp[t++] = A[i++];
+        }
+
+        while (j <= r) {
+            tmp[t++] = A[j++];
+        }
 
         i = p;
         t = 0;
+
         while (i <= r) {
-            A[i++] = tmp[t++];
+            A[i] = tmp[t++];
             count++;
-            if (count == K) result = A[i - 1]; // i-1이 방금 저장한 위치
+
+            if (count == K) {
+                result = A[i];
+            }
+            i++;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        A = new int[N];
-        tmp = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) A[i] = Integer.parseInt(st.nextToken());
-
-        mergeSort(0, N - 1);
-        System.out.println(result);
     }
 }
