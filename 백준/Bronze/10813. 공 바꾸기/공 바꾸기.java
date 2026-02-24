5a29ca3bf281bd5f99a626ca7811dbd3a604c8bc
Minseong <kcm1042@naver.com>
2026-02-24 10:16:48 +0900

    [Bronze II] Title: 공 바꾸기, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,38 +1,51 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-
-    public static void main(String[] args){
-
-        Scanner scanner = new Scanner(System.in);
-
-        // N: 바구니의 개수, M: 교환 횟수
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-
-        // 바구니의 초기 상태 배열 생성.
-
-        int[] baskets = new int[N];
-
-        for (int i=0; i<N; i++){
-            baskets[i] = i + 1;
+public class Main {
+    
+    public static int[] baguni;
+    
+    public static void swap (int ball1, int ball2) {
+        
+        int temp = baguni[ball1];      // 1번 공을 temp에 넣고
+        baguni[ball1] = baguni[ball2]; // 1번에 2번을 넣고
+        baguni[ball2] = temp;  // 2번에 1번을 넣음
+    
+    }
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        
+        int n = Integer.parseInt(inputs[0]); // 바구니 개수
+        int m = Integer.parseInt(inputs[1]); // m번 교환
+        
+        baguni = new int[n+1]; // 1-based indexing을 위해서 +1
+        
+        // 바구니 데이터 초기화
+        for (int i=1; i<n+1; i++) {
+            baguni[i] = i;
         }
+        
+        for (int i=0; i<m; i++) {
 
-        //교환 횟수만큼 반복
-        for (int k=0; k<M; k++){
-            int i = scanner.nextInt() - 1;
-            int j = scanner.nextInt() - 1;
-
-            // 바구니 교환
-            int temp = baskets[i];
-            baskets[i] = baskets[j];
-            baskets[j] = temp;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int ball1 = Integer.parseInt(st.nextToken());
+            int ball2 = Integer.parseInt(st.nextToken());
+            
+            swap(ball1,ball2);
+            
         }
-
-        // 최종 바구니 상태 출력
-        for (int ball : baskets) {
-            System.out.print(ball + " ");
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i=1; i<n+1; i++) {
+            sb.append(baguni[i]).append(" ");
         }
+        
+        System.out.println(sb.toString());
         
     }
 }
