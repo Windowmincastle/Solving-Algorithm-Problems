b092ddd65aecabb845d97b335aa3ecf889868b0e
Minseong <kcm1042@naver.com>
2026-02-26 17:16:49 +0900

    [Silver IV] Title: 한수, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,39 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
+public class Main {
+    
+    public static boolean isHansu(int num) {
+        
+        if (num < 100) return true;
+        if (num == 1000) return false;
+        
+        // 세자릿수만 판별하면된다.
+        int hundreds = num / 100; // 백의 자리
+        int tens = (num / 10) % 10; // 십의 자리
+        int ones = num % 10; // 일의 자리
+        
+        if ((hundreds - tens) == (tens-ones)) return true;
+        
+        return false;
+    }
+    
     
     public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int n = Integer.parseInt(br.readLine());
+        
         int count = 0;
         
-        for(int i=1; i<=n; i++){
+        for (int i=1; i<=n; i++) {
             
-            if( i < 100 ){
+            if(isHansu(i)){
                 count++;
-                continue;
-            }
-            
-            String str = String.valueOf(i);
-            
-            int[] numbers = new int[str.length()];
-            
-            for(int j = 0; j< str.length(); j++){
-                
-                numbers[j] = str.charAt(j) - '0';
-            }
-            
-            int standard = numbers[1] - numbers[0];
-            for (int k = 2; k<str.length(); k++){
-                
-                if ( numbers[k] - numbers[k-1] == standard ){
-                    count++;
-                }
             }
         }
+        
         System.out.println(count);
     }
     
