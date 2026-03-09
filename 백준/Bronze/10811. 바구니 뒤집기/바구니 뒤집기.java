ee702fdd84559e06727770bd0817c94da0eb1b58
Minseong <kcm1042@naver.com>
2026-03-09 16:50:30 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,41 +1,50 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int m = input.nextInt();
-
-        int[] lst = new int[n];
-
-        // 초기 바구니 순서 설정
-        for (int i = 0; i < n; i++) {
-            lst[i] = i + 1;
+    
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        
+        int[] ary = new int[n];
+        
+        for (int i=0; i<ary.length; i++) {
+            ary[i] = i+1;
         }
-
-        // M번 바구니의 순서를 역순으로 만들기
-        for (int k = 0; k < m; k++) {
-            int i = input.nextInt();
-            int j = input.nextInt();
-
-            reverse(lst, i - 1, j - 1);
+        
+        for (int k=0; k<m; k++) {
+            
+            String[] range = br.readLine().split(" ");
+            int i = Integer.parseInt(range[0]);
+            int j = Integer.parseInt(range[1]);
+            
+            int left = i - 1;
+            int right = j - 1;
+            
+            while (left < right) {
+                int temp = ary[left];
+                ary[left] = ary[right];
+                ary[right] = temp;
+            
+                left++;
+                right--;
+            }
+            
         }
-
-        // 결과 출력
-        for (int num : lst) {
-            System.out.print(num + " ");
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int num : ary ) {
+            
+            sb.append(num).append(" ");
+            
         }
-    }
-
-    // 배열의 일부를 역순으로 만드는 메서드
-    private static void reverse(int[] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;
-        }
+        
+        System.out.println(sb.toString());
+        
     }
 }
