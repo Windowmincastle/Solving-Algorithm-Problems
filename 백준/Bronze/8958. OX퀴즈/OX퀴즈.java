174aeada924009826aef83f81d4513f95f863b77
Minseong <kcm1042@naver.com>
2026-04-01 17:10:48 +0900

    [Bronze II] Title: OX퀴즈, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -2,35 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    public static void main(String[] args) throws Exception {
-        
+
+    public static void main(String[] args) throws  Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
-        int TC = Integer.parseInt(br.readLine());
-        
-        for (int i=0; i<TC; i++) {
-            
-            int score = 1;
-            int scoreSum = 0;
-            char[] oxAry = br.readLine().toCharArray();
-            
-            for(int j=0; j<oxAry.length; j++) {
-                
-                if( oxAry[j] == 'O') {
-                    scoreSum += score;
-                    score++;
-                } else {
-                    score = 1;
-                    continue;
+
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<tc; i++) {
+
+            int score = 0;
+            String input = br.readLine();
+            char[] toCharAry = input.toCharArray();
+            int check = 0;
+
+            for (char ox : toCharAry) {
+
+                char o = 'O';
+                char x = 'X';
+
+                if (ox == o) {
+                    score += ( 1 + check );
+                    check++;
+                } else if ( ox == x) {
+                    check = 0;
                 }
-                
             }
-            sb.append(scoreSum).append("\n");
+
+            sb.append(score).append(" ");
         }
-        
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
-    
 }
