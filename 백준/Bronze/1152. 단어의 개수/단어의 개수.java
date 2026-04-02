af14df433c95a50dda89db8cd7a0c4fabe779254
Minseong <kcm1042@naver.com>
2026-04-02 16:59:28 +0900

    [Bronze II] Title: 단어의 개수, Time: 268 ms, Memory: 52772 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,19 +1,22 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+// 문제 이름 : 단어의 개수
+// 문제 번호 : 1152
 public class Main {
-    
+
     public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine().trim();
         
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-String line = br.readLine().trim();
-
-if (line.isEmpty()) {
-    System.out.println(0);
-} else {
-    System.out.println(line.split(" ").length);
-}
-
+        if(input.isEmpty()) {
+            System.out.println(0);    
+        } else {
+            String[] toDivide = input.split(" ");
+            System.out.println(toDivide.length);
+        }
         
     }
 }
