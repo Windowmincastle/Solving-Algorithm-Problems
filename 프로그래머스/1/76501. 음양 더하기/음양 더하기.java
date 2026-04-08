8de3c201735e0dce354dd67f0e3cf97c799762e0
Minseong <kcm1042@naver.com>
2026-04-08 10:37:44 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,16 +1,21 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        
+
         int answer = 0;
         
-        for (int i=0; i<absolutes.length; i++){
+        if (signs[0]) {
+            answer = absolutes[0];
+        } else {
+            answer = -absolutes[0];
+        }
+        
+        for (int i=1; i<absolutes.length; i++) {
             
-            if (signs[i] == true) {
+            if(signs[i]) {
                 answer += absolutes[i];
             } else {
-                answer -= absolutes[i];
+                answer += -absolutes[i];
             }
-            
         }
         
         return answer;
