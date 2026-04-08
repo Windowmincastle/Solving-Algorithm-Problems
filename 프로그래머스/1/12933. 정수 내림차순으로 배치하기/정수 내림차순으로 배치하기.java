c392bb632386d141a74f198eff51417b81cadbaa
Minseong <kcm1042@naver.com>
2026-04-08 10:15:49 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.58 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,29 +1,14 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public long solution(long n) {
         
-        // 118372의 각 자릿수를 잘라서 오름차순 정렬해야함.
+        char[] words = String.valueOf(n).toCharArray();
+        Arrays.sort(words);
         
-        // 1. n을 String으로 변환하자
-        String strConvert = String.valueOf(n);
-        int len = strConvert.length();
-        // 2. int[] 타입으로 바꿔서 담자.
-        int[] intConvert = new int[len];
+        StringBuilder sb = new StringBuilder(new String(words));
+        sb.reverse();
         
-        for (int i=0; i<intConvert.length; i++) {
-            intConvert[i] = strConvert.charAt(i) - '0';
-        }
-        //3. 오름차순 정렬하자.
-        Arrays.sort(intConvert);
-        //4. 문자열로 바꾸자.
-        
-        strConvert = "";
-        for (int j=intConvert.length-1; j >= 0; j--) {
-            strConvert += String.valueOf(intConvert[j]);
-        }
-        long answer = Long.parseLong(strConvert);
-        return answer;
+        return Long.parseLong(sb.toString());
     }
 }
